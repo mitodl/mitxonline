8168920fbcb93dd158fad4b420bec5bf9d33f549
Umar Asghar <umar.asghar@arbisoft.com>
2021-08-24 15:59:19 +0500

    make product page faculty memebers optional (#122)

--- cms/migrations/0010_coursepage_video_url.py
+++ cms/migrations/0010_coursepage_video_url.py
@@ -6,13 +6,17 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cms', '0009_product_faculty_section'),
+        ("cms", "0009_product_faculty_section"),
     ]
 
     operations = [
         migrations.AddField(
-            model_name='coursepage',
-            name='video_url',
-            field=models.URLField(blank=True, help_text='URL to the video to be displayed for this program/course. It can be an HLS or Youtube video URL.', null=True),
+            model_name="coursepage",
+            name="video_url",
+            field=models.URLField(
+                blank=True,
+                help_text="URL to the video to be displayed for this program/course. It can be an HLS or Youtube video URL.",
+                null=True,
+            ),
         ),
     ]
