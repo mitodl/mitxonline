33021d90dbda339787c11ab27a02ae0fcb0aca5e
asadiqbal08 <asad.iqbal@arbisoft.com>
2021-09-01 16:24:16 +0500

    Added a JS test

--- static/js/containers/pages/DashboardPage_test.js
+++ static/js/containers/pages/DashboardPage_test.js
@@ -67,8 +67,12 @@ describe("DashboardPage", () => {
   })
 
   it("links to the courseware URL if that property is set on the course run", async () => {
+    const past = moment().add(-10, "days"),
+      future = moment().add(10, "days")
     const exampleCoursewareUrl = "http://example.com/my-course"
     userEnrollments[0].run.courseware_url = exampleCoursewareUrl
+    userEnrollments[0].run.start_date = past.toISOString()
+    userEnrollments[0].run.end_date = future.toISOString()
     userEnrollments[1].run.courseware_url = null
     const { inner } = await renderPage({
       entities: { enrollments: userEnrollments }
@@ -79,6 +83,21 @@ describe("DashboardPage", () => {
     const unlinkedTitle = enrolledItems.at(1).find("h4")
     assert.isTrue(linkedTitle.find("a").exists())
     assert.equal(linkedTitle.find("a").prop("href"), exampleCoursewareUrl)
+    assert.isFalse(unlinkedTitle.find("a").exists())
+  })
+
+  it("Not links to the courseware URL if run is started in future", async () => {
+    const future = moment().add(10, "days")
+    const exampleCoursewareUrl = "http://example.com/my-course"
+    userEnrollments[0].run.courseware_url = exampleCoursewareUrl
+    userEnrollments[0].run.start_date = future.toISOString()
+    userEnrollments[0].run.end_date = future.toISOString()
+    const { inner } = await renderPage({
+      entities: { enrollments: userEnrollments }
+    })
+    const enrolledItems = inner.find(".enrolled-item")
+    assert.lengthOf(enrolledItems, userEnrollments.length)
+    const unlinkedTitle = enrolledItems.at(0).find("h4")
     assert.isFalse(unlinkedTitle.find("a").exists())
   })
 
