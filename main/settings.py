7049dd8168bb56edf16b9dc665144a5dbdcc9028
Arslan Ashraf <arslan.ashraf@arbisoft.com>
2021-09-07 12:31:34 +0500

    add/enable GTM support for basic events

--- main/settings.py
+++ main/settings.py
@@ -233,6 +233,7 @@ TEMPLATES = [
                 "django.contrib.messages.context_processors.messages",
                 "social_django.context_processors.backends",
                 "social_django.context_processors.login_redirect",
+                "main.context_processors.api_keys",
             ]
         },
     }
