ebe972ba4ae6973fde170bad803f6a05ded0532a
Umar Asghar <umar.asghar@arbisoft.com>
2021-09-09 11:42:10 +0500

    improve top-bar menu (#135)
    improve top-bar menu

--- static/js/components/TopAppBar.js
+++ static/js/components/TopAppBar.js
@@ -3,8 +3,8 @@
 import React from "react"
 
 import { routes } from "../lib/urls"
-import MixedLink from "./MixedLink"
 import UserMenu from "./UserMenu"
+import AnonymousMenu from "./AnonymousMenu"
 import type { Location } from "react-router"
 
 import type { CurrentUser } from "../flow/authTypes"
@@ -48,88 +48,27 @@ const TopAppBar = ({ currentUser, location }: Props) => (
           <span className="bar" />
           <span className="bar" />
         </button>
-        <ul
+        <div
           id="nav"
           className={`${
             currentUser.is_authenticated ? "" : "collapse"
           } navbar-collapse px-0 justify-content-end`}
         >
-          {currentUser.is_authenticated ? (
-            <React.Fragment>
-              <li>
-                <UserMenu currentUser={currentUser} />
-              </li>
-              {/* These menu lists will show/hide based on desktop/mobile screen. */}
-              <li
-                className="authenticated-menu"
-                data-toggle="collapse"
-                data-target="#nav"
-              >
-                <MixedLink dest={routes.profile.view} aria-label="Profile">
-                  Profile
-                </MixedLink>
-              </li>
-              <li
-                className="authenticated-menu"
-                data-toggle="collapse"
-                data-target="#nav"
-              >
-                <MixedLink dest={routes.dashboard} aria-label="Dashboard">
-                  Dashboard
-                </MixedLink>
-              </li>
-              <li
-                className="authenticated-menu"
-                data-toggle="collapse"
-                data-target="#nav"
-              >
-                <MixedLink dest={routes.accountSettings} aria-label="Account">
-                  Account
-                </MixedLink>
-              </li>
-              <li
-                className="authenticated-menu"
-                data-toggle="collapse"
-                data-target="#nav"
-              >
-                <MixedLink dest={routes.logout} aria-label="Sign Out">
-                  Sign Out
-                </MixedLink>
-              </li>
-            </React.Fragment>
-          ) : (
-            <React.Fragment>
-              <li data-toggle="collapse" data-target="#nav">
-                <MixedLink dest={routes.login.begin} aria-label="Login">
-                  Sign In
-                </MixedLink>
-              </li>
-              <li data-toggle="collapse" data-target="#nav">
-                <MixedLink
-                  dest={routes.register.begin}
-                  className="button"
-                  aria-label="Login"
-                >
-                  Create Account
-                </MixedLink>
-              </li>
-              <li>
-                <MixedLink dest={routes.login.begin} aria-label="Login">
-                  Sign In
-                </MixedLink>
-              </li>
-              <li>
-                <MixedLink
-                  dest={routes.register.begin}
-                  className="button"
-                  aria-label="Login"
-                >
-                  Create Account
-                </MixedLink>
-              </li>
-            </React.Fragment>
-          )}
-        </ul>
+          <div className="full-screen-menu">
+            {currentUser.is_authenticated ? (
+              <UserMenu currentUser={currentUser} useScreenOverlay={false} />
+            ) : (
+              <AnonymousMenu useScreenOverlay={false} />
+            )}
+          </div>
+          <div className="mobile-menu">
+            {currentUser.is_authenticated ? (
+              <UserMenu currentUser={currentUser} useScreenOverlay={true} />
+            ) : (
+              <AnonymousMenu useScreenOverlay={true} />
+            )}
+          </div>
+        </div>
       </nav>
     </div>
   </header>
--- static/js/components/UserMenu_test.js
+++ static/js/components/UserMenu_test.js
@@ -9,13 +9,29 @@ import { makeUser } from "../factories/user"
 
 describe("UserMenu component", () => {
   const user = makeUser()
+  it("has the correct number of menu links", () => {
+    assert.isOk(
+      shallow(<UserMenu currentUser={user} useScreenOverlay={false} />)
+        .find("MixedLink")
+        .exists()
+    )
+  })
+
+  it("has the correct number of menu links in the mobile view", () => {
+    assert.equal(
+      shallow(<UserMenu currentUser={user} useScreenOverlay={true} />).find(
+        ".authenticated-menu"
+      ).length,
+      4
+    )
+  })
 
   it("has a link to logout", () => {
     assert.equal(
-      shallow(<UserMenu currentUser={user} />)
-        .find("a")
-        .at(0)
-        .prop("href"),
+      shallow(<UserMenu currentUser={user} useScreenOverlay={false} />)
+        .find("MixedLink")
+        .at(3)
+        .prop("dest"),
       routes.logout
     )
   })
