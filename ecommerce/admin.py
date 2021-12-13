b00ff1e13e8050f7eed07a46f0493de39e42b345
James Kachel <james@jkachel.com>
2021-12-13 13:50:13 -0600

    Ran formatter on admin.py

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from ecommerce.models import Product
 from reversion.admin import VersionAdmin
 
+
 @admin.register(Product)
 class ProductAdmin(VersionAdmin):
     """Admin for Product"""
