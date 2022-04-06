fae0d13c4da63e08109abeb7bcae94d9a16b4e56
Nathan Levesque <rhysyngsun@gmail.com>
2022-04-06 11:23:20 -0400

    Revert "Add support for yarn workspaces"
    This reverts commit 706f65af3243dc3b5367f17325e47dc364f36fab.

---
+++ static/js/entry/header.js
@@ -0,0 +1,78 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import { Provider } from "react-redux"
+import { AppContainer } from "react-hot-loader"
+import { Router as ReactRouter } from "react-router"
+import { createBrowserHistory } from "history"
+
+import configureStore from "../store/configureStore"
+import { AppTypeContext, MIXED_APP_CONTEXT } from "../contextDefinitions"
+import HeaderApp from "../containers/HeaderApp"
+import ProductDetailEnrollApp from "../containers/ProductDetailEnrollApp"
+import UpsellCardApp from "../containers/UpsellCardApp"
+// Object.entries polyfill
+import entries from "object.entries"
+
+require("react-hot-loader/patch")
+/* global SETTINGS:false */
+__webpack_public_path__ = SETTINGS.public_path // eslint-disable-line no-undef, camelcase
+
+if (!Object.entries) {
+  entries.shim()
+}
+
+const store = configureStore()
+
+const rootEl = document.getElementById("header")
+
+const renderHeader = () => {
+  const history = createBrowserHistory()
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={store}>
+        <AppTypeContext.Provider value={MIXED_APP_CONTEXT}>
+          <ReactRouter history={history}>
+            <HeaderApp />
+          </ReactRouter>
+        </AppTypeContext.Provider>
+      </Provider>
+    </AppContainer>,
+    rootEl
+  )
+}
+
+const renderEnrollSection = (courseId, element, reduxStore) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={reduxStore}>
+        <ProductDetailEnrollApp courseId={courseId} />
+      </Provider>
+    </AppContainer>,
+    element
+  )
+}
+
+const renderUpsellCard = (courseId, element, reduxStore) => {
+  ReactDOM.render(
+    <AppContainer>
+      <Provider store={reduxStore}>
+        <UpsellCardApp courseId={courseId} />
+      </Provider>
+    </AppContainer>,
+    element
+  )
+}
+
+renderHeader()
+
+document.addEventListener("DOMContentLoaded", function() {
+  const enrollSectionEl = document.getElementById("productDetailEnrollment")
+  const upsellCardEl = document.getElementById("upsellCard")
+  const courseIdEl = document.getElementById("courseId")
+  if (enrollSectionEl && courseIdEl && upsellCardEl) {
+    const productDetailStore = configureStore()
+    const courseId = courseIdEl.value
+    renderEnrollSection(courseId, enrollSectionEl, productDetailStore)
+    renderUpsellCard(courseId, upsellCardEl, productDetailStore)
+  }
+})
---
+++ static/js/lib/notificationsApi.js
@@ -0,0 +1,126 @@
+/* global SETTINGS: false */
+import { getCookie, removeCookie } from "./api"
+import { isEmptyText } from "./util"
+import {
+  ALERT_TYPE_DANGER,
+  ALERT_TYPE_SUCCESS,
+  USER_MSG_COOKIE_NAME,
+  USER_MSG_TYPE_COMPLETED_AUTH,
+  USER_MSG_TYPE_ENROLL_BLOCKED,
+  USER_MSG_TYPE_ENROLL_FAILED,
+  USER_MSG_TYPE_ENROLLED,
+  USER_MSG_TYPE_PAYMENT_DECLINED,
+  USER_MSG_TYPE_PAYMENT_CANCELLED,
+  USER_MSG_TYPE_PAYMENT_ERROR_UNKNOWN,
+  USER_MSG_TYPE_PAYMENT_ACCEPTED,
+  USER_MSG_TYPE_PAYMENT_ACCEPTED_NO_VALUE,
+  USER_MSG_TYPE_PAYMENT_REVIEW
+} from "../constants"
+
+type UserMessage = {
+  type: string,
+  text: string
+}
+
+export function getStoredUserMessage(): UserMessage | null {
+  const userMsgValue = getCookie(USER_MSG_COOKIE_NAME)
+  if (!userMsgValue || isEmptyText(userMsgValue)) {
+    return null
+  }
+  const userMsgObject = JSON.parse(decodeURIComponent(userMsgValue))
+  return parseStoredUserMessage(userMsgObject)
+}
+
+export function parseStoredUserMessage(
+  userMsgJson: Object
+): UserMessage | null {
+  const msgType = userMsgJson.type || null
+  if (!msgType) {
+    return null
+  }
+  let alertType, msgText
+  switch (msgType) {
+  case USER_MSG_TYPE_ENROLLED:
+    alertType = ALERT_TYPE_SUCCESS
+    msgText = userMsgJson.run
+      ? `Success! You've been enrolled in ${userMsgJson.run}.`
+      : null
+    break
+  case USER_MSG_TYPE_ENROLL_FAILED:
+    alertType = ALERT_TYPE_DANGER
+    msgText = `Something went wrong with your enrollment. Please contact support at ${
+      SETTINGS.support_email
+    }.`
+    break
+  case USER_MSG_TYPE_ENROLL_BLOCKED:
+    alertType = ALERT_TYPE_DANGER
+    msgText =
+        "We're sorry, your country is currently blocked from enrolling in this course."
+    break
+  case USER_MSG_TYPE_COMPLETED_AUTH:
+    alertType = ALERT_TYPE_SUCCESS
+    msgText = "Account created!"
+    break
+  case USER_MSG_TYPE_PAYMENT_DECLINED:
+    alertType = ALERT_TYPE_DANGER
+    msgText = "Payment was declined, please try again."
+    break
+  case USER_MSG_TYPE_PAYMENT_ERROR_UNKNOWN:
+    alertType = ALERT_TYPE_DANGER
+    msgText = "Unknown error trying to complete order."
+    break
+  case USER_MSG_TYPE_PAYMENT_CANCELLED:
+    alertType = ALERT_TYPE_DANGER
+    msgText = "Payment was cancelled."
+    break
+  case USER_MSG_TYPE_PAYMENT_REVIEW:
+    // Probably won't hit this in production, but here to cover all possible cases
+    alertType = ALERT_TYPE_SUCCESS
+    msgText = "Payment is pending review."
+    break
+  case USER_MSG_TYPE_PAYMENT_ACCEPTED:
+    alertType = ALERT_TYPE_SUCCESS
+    msgText = userMsgJson.run
+      ? `Success! You are now enrolled for the paid version of ${
+        userMsgJson.run
+      }.`
+      : null
+    break
+  case USER_MSG_TYPE_PAYMENT_ACCEPTED_NO_VALUE:
+    alertType = ALERT_TYPE_SUCCESS
+    msgText = userMsgJson.run
+      ? `Success! You are now enrolled for the certificate version of ${
+        userMsgJson.run
+      }.`
+      : null
+    break
+  default:
+    return null
+  }
+  if (!alertType || !msgText) {
+    return null
+  }
+  return {
+    type: alertType,
+    text: msgText
+  }
+}
+
+export function removeStoredUserMessage(): null {
+  removeCookie(USER_MSG_COOKIE_NAME)
+}
+
+export const getNotificationAlertProps = (
+  notificationType: string
+): {
+  color?: string
+} => {
+  switch (notificationType) {
+  case ALERT_TYPE_SUCCESS:
+    return { color: ALERT_TYPE_SUCCESS }
+  case ALERT_TYPE_DANGER:
+    return { color: ALERT_TYPE_DANGER }
+  default:
+    return {}
+  }
+}
