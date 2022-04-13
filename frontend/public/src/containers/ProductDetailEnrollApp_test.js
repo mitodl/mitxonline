89689190ed26ec42b2ff0e66fc23e5f36a8ee5fc
Nathan Levesque <rhysyngsun@gmail.com>
2022-04-12 20:57:08 -0400

    Adding back yarn workspaces

---
+++ frontend/public/src/containers/ProductDetailEnrollApp_test.js
@@ -0,0 +1,204 @@
+/* global SETTINGS: false */
+// @flow
+import { assert } from "chai"
+
+import IntegrationTestHelper from "../util/integration_test_helper"
+import ProductDetailEnrollApp, {
+  ProductDetailEnrollApp as InnerProductDetailEnrollApp
+} from "./ProductDetailEnrollApp"
+
+import { courseRunsSelector } from "../lib/queries/courseRuns"
+import {
+  makeCourseRunDetail,
+  makeCourseRunEnrollment
+} from "../factories/course"
+
+import * as courseApi from "../lib/courseApi"
+
+import sinon from "sinon"
+import { makeUser } from "../factories/user"
+
+describe("ProductDetailEnrollApp", () => {
+  let helper, renderPage, isWithinEnrollmentPeriodStub, courseRun, currentUser
+
+  beforeEach(() => {
+    helper = new IntegrationTestHelper()
+    courseRun = makeCourseRunDetail()
+    currentUser = makeUser()
+    courseRun["products"] = [{ id: 1 }]
+    renderPage = helper.configureHOCRenderer(
+      ProductDetailEnrollApp,
+      InnerProductDetailEnrollApp,
+      {
+        entities: {
+          courseRuns:  [courseRun],
+          currentUser: currentUser
+        }
+      },
+      {}
+    )
+    SETTINGS.features = { upgrade_dialog: false, disable_discount_ui: false }
+
+    isWithinEnrollmentPeriodStub = helper.sandbox.stub(
+      courseApi,
+      "isWithinEnrollmentPeriod"
+    )
+  })
+
+  afterEach(() => {
+    helper.cleanup()
+  })
+
+  it("renders a Loader component", async () => {
+    const { inner } = await renderPage({
+      queries: {
+        courseRuns: {
+          isPending: true
+        }
+      }
+    })
+
+    assert.isTrue(inner.props().isLoading)
+    const loader = inner.find("Loader")
+    assert.isOk(loader.exists())
+    assert.isTrue(loader.props().isLoading)
+  })
+
+  it("checks for enroll now button", async () => {
+    const courseRun = makeCourseRunDetail()
+    isWithinEnrollmentPeriodStub.returns(true)
+    const { inner } = await renderPage(
+      {
+        entities: {
+          courseRuns: [courseRun]
+        },
+        queries: {
+          courseRuns: {
+            isPending: false,
+            status:    200
+          }
+        }
+      },
+      {}
+    )
+    assert.equal(
+      inner
+        .find(".enroll-now")
+        .at(0)
+        .text(),
+      "Enroll now"
+    )
+  })
+
+  it("checks for enroll now button should not appear if enrollment start in future", async () => {
+    const courseRun = makeCourseRunDetail()
+    isWithinEnrollmentPeriodStub.returns(false)
+    const { inner } = await renderPage(
+      {
+        entities: {
+          courseRuns: [courseRun]
+        },
+        queries: {
+          courseRuns: {
+            isPending: false,
+            status:    200
+          }
+        }
+      },
+      {}
+    )
+
+    assert.isNotOk(
+      inner
+        .find(".enroll-now")
+        .at(0)
+        .exists()
+    )
+  })
+
+  it("checks for enroll now button should appear if enrollment start not in future", async () => {
+    const courseRun = makeCourseRunDetail()
+    isWithinEnrollmentPeriodStub.returns(true)
+    const { inner } = await renderPage(
+      {
+        entities: {
+          courseRuns: [courseRun]
+        },
+        queries: {
+          courseRuns: {
+            isPending: false,
+            status:    200
+          }
+        }
+      },
+      {}
+    )
+
+    assert.equal(
+      inner
+        .find(".enroll-now")
+        .at(0)
+        .text(),
+      "Enroll now"
+    )
+  })
+
+  it("checks for enrolled button", async () => {
+    const userEnrollment = makeCourseRunEnrollment()
+    const expectedResponse = {
+      ...userEnrollment.run,
+      is_enrolled: true
+    }
+
+    const { inner, store } = await renderPage(
+      {
+        entities: {
+          courseRuns: [expectedResponse]
+        },
+        queries: {
+          courseRuns: {
+            isPending: false,
+            status:    200
+          }
+        }
+      },
+      {}
+    )
+
+    assert.equal(
+      inner
+        .find("a")
+        .at(0)
+        .text(),
+      "Enrolled ✓"
+    )
+    assert.equal(courseRunsSelector(store.getState())[0], expectedResponse)
+  })
+  ;[
+    [true, 201],
+    [false, 400]
+  ].forEach(([success, returnedStatusCode]) => {
+    it(`shows dialog to upgrade user enrollment and handles ${returnedStatusCode} response`, async () => {
+      isWithinEnrollmentPeriodStub.returns(true)
+      SETTINGS.features.upgrade_dialog = true
+      const { inner } = await renderPage()
+
+      sinon.assert.calledWith(
+        helper.handleRequestStub,
+        "/api/course_runs/?relevant_to=",
+        "GET"
+      )
+      sinon.assert.calledWith(helper.handleRequestStub, "/api/users/me", "GET")
+
+      const enrollBtn = inner.find(".enroll-now").at(0)
+      assert.isTrue(enrollBtn.exists())
+      await enrollBtn.prop("onClick")()
+
+      const modal = inner.find(".upgrade-enrollment-modal")
+      const upgradeForm = modal.find("form").at(0)
+      assert.isTrue(upgradeForm.exists())
+
+      assert.equal(upgradeForm.find("input[type='hidden']").prop("value"), "1")
+    })
+  })
+})
---
+++ frontend/public/src/containers/pages/checkout/CartPage.js
@@ -0,0 +1,241 @@
+// @flow
+/* global SETTINGS: false */
+import React from "react"
+import DocumentTitle from "react-document-title"
+import {
+  ALERT_TYPE_DANGER,
+  ALERT_TYPE_SUCCESS,
+  CART_DISPLAY_PAGE_TITLE
+} from "../../../constants"
+import { compose } from "redux"
+import { connect } from "react-redux"
+import { connectRequest, mutateAsync } from "redux-query"
+import { createStructuredSelector } from "reselect"
+import { pathOr } from "ramda"
+
+import type { BasketItem, Discount, Refund } from "../../../flow/cartTypes"
+
+import Loader from "../../../components/Loader"
+import { CartItemCard } from "../../../components/CartItemCard"
+import { OrderSummaryCard } from "../../../components/OrderSummaryCard"
+
+import {
+  cartQuery,
+  cartQueryKey,
+  cartSelector,
+  totalPriceSelector,
+  discountedPriceSelector,
+  discountSelector,
+  applyDiscountCodeMutation,
+  clearDiscountCodeMutation
+} from "../../../lib/queries/cart"
+
+import type { RouterHistory } from "react-router"
+import { isSuccessResponse } from "../../../lib/util"
+import { addUserNotification } from "../../../actions"
+
+type Props = {
+  history: RouterHistory,
+  cartItems: Array<BasketItem>,
+  totalPrice: number,
+  discountedPrice: number,
+  discounts: Array<Discount>,
+  isLoading: boolean,
+  applyDiscountCode: (code: string) => Promise<any>,
+  clearDiscountCode: () => Promise<any>,
+  addUserNotification: Function,
+  forceRequest: Function
+}
+
+type CartState = {
+  discountCode: string,
+  discountCodeIsBad: boolean
+}
+
+export class CartPage extends React.Component<Props, CartState> {
+  state = {
+    discountCode:      "",
+    discountCodeIsBad: false
+  }
+
+  async clearDiscount() {
+    const { forceRequest, addUserNotification, clearDiscountCode } = this.props
+
+    let userMessage, messageType
+
+    const resp = await clearDiscountCode()
+
+    if (isSuccessResponse(resp)) {
+      messageType = ALERT_TYPE_SUCCESS
+      userMessage = "Discount code cleared."
+
+      forceRequest()
+      this.setState({ discountCodeIsBad: false, discountCode: "" })
+    } else {
+      // TODO: this should use the banner thingy
+      messageType = ALERT_TYPE_DANGER
+      userMessage = `Something went wrong when trying to clear your discount code. Please contact support at ${SETTINGS.support_email}.`
+    }
+
+    addUserNotification({
+      "clear-discount-notification": {
+        type:  messageType,
+        props: {
+          text: userMessage
+        }
+      }
+    })
+  }
+
+  async addDiscount(ev: Object) {
+    const subbedCode = ev.couponCode
+    const { applyDiscountCode, forceRequest, addUserNotification } = this.props
+
+    this.setState({ discountCode: subbedCode, discountCodeIsBad: false })
+
+    if (String(subbedCode).trim().length === 0) {
+      return
+    }
+
+    let userMessage, messageType
+
+    const resp = await applyDiscountCode(this.state.discountCode)
+    if (isSuccessResponse(resp)) {
+      messageType = ALERT_TYPE_SUCCESS
+      userMessage = "Discount code added."
+
+      forceRequest()
+    } else {
+      messageType = ALERT_TYPE_DANGER
+      userMessage = `Discount code ${this.state.discountCode} is invalid.`
+
+      this.setState({ discountCodeIsBad: true })
+    }
+  }
+  renderCartItemCard(cartItem: BasketItem) {
+    return (
+      <CartItemCard
+        key={`cartsummarycard_${cartItem.product.id}`}
+        product={cartItem.product}
+      />
+    )
+  }
+
+  renderEmptyCartCard() {
+    return (
+      <div
+        className="enrolled-item container card mb-4 rounded-0 flex-grow-1"
+        key="cartsummarycard"
+      >
+        <div className="row d-flex flex-sm-columm p-md-3">
+          <div className="flex-grow-1 mx-3 d-sm-flex flex-column">
+            <div className="detail">Your cart is empty.</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+  renderOrderSummaryCard() {
+    const { totalPrice, discountedPrice, discounts } = this.props
+    const refunds = []
+
+    return (
+      <OrderSummaryCard
+        totalPrice={totalPrice}
+        orderFulfilled={false}
+        discountedPrice={discountedPrice}
+        discounts={discounts}
+        refunds={refunds}
+        clearDiscount={this.clearDiscount.bind(this)}
+        addDiscount={this.addDiscount.bind(this)}
+        discountCodeIsBad={this.state.discountCodeIsBad}
+        discountCode={this.state.discountCode}
+      />
+    )
+  }
+
+  render() {
+    const { cartItems, isLoading } = this.props
+
+    return (
+      <DocumentTitle
+        title={`${SETTINGS.site_name} | ${CART_DISPLAY_PAGE_TITLE}`}
+      >
+        <Loader isLoading={isLoading}>
+          <div className="std-page-body cart container">
+            <div className="row">
+              <div className="col-12 d-flex justify-content-between">
+                <h1 className="flex-grow-1">Checkout</h1>
+
+                <p className="text-right d-md-none">
+                  <a
+                    href="https://mitxonline.zendesk.com/hc/en-us"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Help &amp; FAQs
+                  </a>
+                </p>
+              </div>
+            </div>
+
+            <div className="row d-none d-md-flex mb-3">
+              <div className="col-md-8">
+                <h4 className="font-weight-normal">
+                  You are about to purchase the following:
+                </h4>
+              </div>
+              <div className="col-md-4 text-right">
+                <h4 className="font-weight-normal">
+                  <a
+                    href="https://mitxonline.zendesk.com/hc/en-us"
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Help &amp; FAQs
+                  </a>
+                </h4>
+              </div>
+            </div>
+
+            <div className="row d-flex flex-column-reverse flex-md-column flex-lg-row">
+              <div className="col-lg-8 enrolled-items">
+                {cartItems && cartItems.length > 0
+                  ? cartItems.map(this.renderCartItemCard.bind(this))
+                  : this.renderEmptyCartCard()}
+              </div>
+              <div className="col-lg-4">{this.renderOrderSummaryCard()}</div>
+            </div>
+          </div>
+        </Loader>
+      </DocumentTitle>
+    )
+  }
+}
+
+const applyDiscountCode = (code: string) =>
+  mutateAsync(applyDiscountCodeMutation(code))
+
+const clearDiscountCode = () => mutateAsync(clearDiscountCodeMutation())
+
+const mapStateToProps = createStructuredSelector({
+  cartItems:       cartSelector,
+  totalPrice:      totalPriceSelector,
+  discountedPrice: discountedPriceSelector,
+  discounts:       discountSelector,
+  isLoading:       pathOr(true, ["queries", cartQueryKey, "isPending"])
+})
+
+const mapDispatchToProps = {
+  addUserNotification,
+  applyDiscountCode,
+  clearDiscountCode
+}
+
+const mapPropsToConfig = () => [cartQuery()]
+
+export default compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  connectRequest(mapPropsToConfig)
+)(CartPage)
