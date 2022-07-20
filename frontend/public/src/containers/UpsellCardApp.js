ba4468576281ba845b12862025d5e3b3acf13c12
Collin Preston <collin_preston@yahoo.com>
2022-07-20 13:49:02 -0400

    Fix tests

--- frontend/public/src/containers/UpsellCardApp.js
+++ frontend/public/src/containers/UpsellCardApp.js
@@ -64,7 +64,7 @@ export class UpsellCardApp extends React.Component<Props, ProductDetailState> {
             <h2>Get a certificate</h2>
           </div>
           <div className="text-right align-self-end">
-            <h2>${product.price - flexPriceDiscountAmount ?? null}</h2>
+            <h2>${product.price - flexPriceDiscountAmount}</h2>
           </div>
         </div>
         <div className="row">
