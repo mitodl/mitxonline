e8b18125d364baa4544b8bd99dbf42b6a2550993
Collin Preston <collin_preston@yahoo.com>
2022-07-28 10:40:22 -0400

    Format

--- cms/migrations/0019_remove_flexiblepricingrequestform_thank_you_text.py
+++ cms/migrations/0019_remove_flexiblepricingrequestform_thank_you_text.py
@@ -6,12 +6,12 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('cms', '0018_add_country_field_for_flexible_pricing'),
+        ("cms", "0018_add_country_field_for_flexible_pricing"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='flexiblepricingrequestform',
-            name='thank_you_text',
+            model_name="flexiblepricingrequestform",
+            name="thank_you_text",
         ),
     ]
