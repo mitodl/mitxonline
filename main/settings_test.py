c854efd3c258eccef493bc1786f577883bad0a1d
Anna Gavrilman <annagav@mit.edu>
2022-08-05 11:58:31 -0400

    black

--- main/settings_test.py
+++ main/settings_test.py
@@ -33,9 +33,7 @@ def cleanup_settings():
     importlib.reload(
         sys.modules["mitol.google_sheets_refunds.settings.google_sheets_refunds"]
     )
-    importlib.reload(
-        sys.modules["mitol.payment_gateway.settings.cybersource"]
-    )
+    importlib.reload(sys.modules["mitol.payment_gateway.settings.cybersource"])
 
 
 class TestSettings(TestCase):
