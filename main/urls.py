286129ea240316da554270a1f6789b14e55ee49b
ALi Raza <77275478+aliraza-abbasi@users.noreply.github.com>
2022-08-10 20:32:48 +0500

    Black formatting

--- main/urls.py
+++ main/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.conf import settings
 from django.conf.urls import include
 from django.conf.urls.static import static
@@ -31,68 +30,65 @@ WAGTAIL_IMG_CACHE_AGE = 31_536_000  # 1 year
 handler500 = "main.views.handler500"
 handler404 = "main.views.handler404"
 
-urlpatterns = (
-    [
-        # NOTE: we only bring in base_urlpatterns so applications can only be created via django-admin
-        path(
-            "oauth2/",
-            include(
-                (base_urlpatterns + oidc_urlpatterns, "oauth2_provider"),
-                namespace="oauth2_provider",
-            ),
+urlpatterns = [
+    # NOTE: we only bring in base_urlpatterns so applications can only be created via django-admin
+    path(
+        "oauth2/",
+        include(
+            (base_urlpatterns + oidc_urlpatterns, "oauth2_provider"),
+            namespace="oauth2_provider",
         ),
-        path("admin/", admin.site.urls),
-        path("status/", include("server_status.urls")),
-        path("hijack/", include("hijack.urls")),
-        path("robots.txt", include("robots.urls")),
-        path("", include("authentication.urls")),
-        path("", include("openedx.urls")),
-        path("", include("mail.urls")),
-        path("", include("users.urls")),
-        path("", include("courses.urls")),
-        path("", include("ecommerce.urls")),
-        path("", include("flexiblepricing.urls")),
-        path("", include("mitol.google_sheets.urls")),
-        re_path(r"^dashboard/", index, name="user-dashboard"),
-        # social django needs to be here to preempt the login
-        path("", include("social_django.urls", namespace="social")),
-        re_path(r"^staff-dashboard/", refine, name="staff-dashboard"),
-        path("signin/", index, name="login"),
-        path("signin/password/", index, name="login-password"),
-        re_path(r"^signin/forgot-password/$", index, name="password-reset"),
-        re_path(
-            r"^signin/forgot-password/confirm/(?P<uid>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/$",
-            index,
-            name="password-reset-confirm",
-        ),
-        path("create-account/", index, name="signup"),
-        path("create-account/details/", index, name="signup-details"),
-        path("create-account/denied/", index, name="signup-denied"),
-        path("create-account/error/", index, name="signup-error"),
-        path("create-account/confirm/", index, name="register-confirm"),
-        path("account/inactive/", index, name="account-inactive"),
-        path("account/confirm-email/", index, name="account-confirm-email-change"),
-        path("profile/", index, name="view-profile"),
-        path("profile/edit/", index, name="edit-profile"),
-        re_path(r"^account-settings/", index, name="account-settings"),
-        re_path(r"^cart/.*", index, name="cart"),
-        re_path(r"^orders/history/.*", index, name="order-history"),
-        re_path(r"^orders/receipt/.*", index, name="order-receipt"),
-        # Wagtail
-        re_path(
-            r"^cms/login", cms_signin_redirect_to_site_signin, name="wagtailadmin_login"
-        ),
-        re_path(r"^cms/", include(wagtailadmin_urls)),
-        re_path(r"^documents/", include(wagtaildocs_urls)),
-        path("", include(wagtail_urls)),
-        path("", include("cms.urls")),
-        # Example view
-        path("", index, name="main-index"),
-    ]
-    + (
-        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    )
+    ),
+    path("admin/", admin.site.urls),
+    path("status/", include("server_status.urls")),
+    path("hijack/", include("hijack.urls")),
+    path("robots.txt", include("robots.urls")),
+    path("", include("authentication.urls")),
+    path("", include("openedx.urls")),
+    path("", include("mail.urls")),
+    path("", include("users.urls")),
+    path("", include("courses.urls")),
+    path("", include("ecommerce.urls")),
+    path("", include("flexiblepricing.urls")),
+    path("", include("mitol.google_sheets.urls")),
+    re_path(r"^dashboard/", index, name="user-dashboard"),
+    # social django needs to be here to preempt the login
+    path("", include("social_django.urls", namespace="social")),
+    re_path(r"^staff-dashboard/", refine, name="staff-dashboard"),
+    path("signin/", index, name="login"),
+    path("signin/password/", index, name="login-password"),
+    re_path(r"^signin/forgot-password/$", index, name="password-reset"),
+    re_path(
+        r"^signin/forgot-password/confirm/(?P<uid>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,32})/$",
+        index,
+        name="password-reset-confirm",
+    ),
+    path("create-account/", index, name="signup"),
+    path("create-account/details/", index, name="signup-details"),
+    path("create-account/denied/", index, name="signup-denied"),
+    path("create-account/error/", index, name="signup-error"),
+    path("create-account/confirm/", index, name="register-confirm"),
+    path("account/inactive/", index, name="account-inactive"),
+    path("account/confirm-email/", index, name="account-confirm-email-change"),
+    path("profile/", index, name="view-profile"),
+    path("profile/edit/", index, name="edit-profile"),
+    re_path(r"^account-settings/", index, name="account-settings"),
+    re_path(r"^cart/.*", index, name="cart"),
+    re_path(r"^orders/history/.*", index, name="order-history"),
+    re_path(r"^orders/receipt/.*", index, name="order-receipt"),
+    # Wagtail
+    re_path(
+        r"^cms/login", cms_signin_redirect_to_site_signin, name="wagtailadmin_login"
+    ),
+    re_path(r"^cms/", include(wagtailadmin_urls)),
+    re_path(r"^documents/", include(wagtaildocs_urls)),
+    path("", include(wagtail_urls)),
+    path("", include("cms.urls")),
+    # Example view
+    path("", index, name="main-index"),
+] + (
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 )
 
 if settings.DEBUG:
