c103430c1605e8a7e5b1057250be1508d1a95a9b
Collin Preston <collin_preston@yahoo.com>
2022-08-23 09:01:44 -0400

    Format

--- cms/migrations/0025_auto_20220823_1237.py
+++ cms/migrations/0025_auto_20220823_1237.py
@@ -7,19 +7,33 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('wagtailimages', '0023_add_choose_permissions'),
-        ('cms', '0024_merge_20220817_2047'),
+        ("wagtailimages", "0023_add_choose_permissions"),
+        ("cms", "0024_merge_20220817_2047"),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='coursepage',
-            name='feature_image',
-            field=models.ForeignKey(blank=True, help_text='Image that will be used where the course is featured or linked. (The recommended dimensions for the image are 375x244)', null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to='wagtailimages.image'),
+            model_name="coursepage",
+            name="feature_image",
+            field=models.ForeignKey(
+                blank=True,
+                help_text="Image that will be used where the course is featured or linked. (The recommended dimensions for the image are 375x244)",
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="+",
+                to="wagtailimages.image",
+            ),
         ),
         migrations.AlterField(
-            model_name='programpage',
-            name='feature_image',
-            field=models.ForeignKey(blank=True, help_text='Image that will be used where the course is featured or linked. (The recommended dimensions for the image are 375x244)', null=True, on_delete=django.db.models.deletion.SET_NULL, related_name='+', to='wagtailimages.image'),
+            model_name="programpage",
+            name="feature_image",
+            field=models.ForeignKey(
+                blank=True,
+                help_text="Image that will be used where the course is featured or linked. (The recommended dimensions for the image are 375x244)",
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                related_name="+",
+                to="wagtailimages.image",
+            ),
         ),
     ]
