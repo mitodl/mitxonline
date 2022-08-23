fbe7be0be7f6bbfb7da34531c11f2553cd8ffbba
Collin Preston <collin_preston@yahoo.com>
2022-08-23 07:18:39 -0400

    UI updates
    Move the | after the program ID to within the Start date check, so it only shows when the start date exists.  Only show the course ID and not the full course ID.  Add formatting (background, padding) to the course upgrade description.

--- frontend/public/src/components/EnrolledItemCard.js
+++ frontend/public/src/components/EnrolledItemCard.js
@@ -361,9 +361,9 @@ export class EnrolledItemCard extends React.Component<
                   <a href={certLocation}>View certificate</a>
                 ) : null}
               </div>
-              <div>
+              <br/>
+              <div className="upgrade-item-description">
                 <p>
-                  <br/>
                   <strong>Upgrade today</strong> and, upon passing, receive your certificate signed by MIT faculty
                   to highlight the knowledge and skills you've gained from this MITx course.
                 </p>
