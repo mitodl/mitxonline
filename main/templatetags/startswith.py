792e0047314723db0ac3ef85e90a6a74f02385ba
ALi Raza <77275478+aliraza-abbasi@users.noreply.github.com>
2022-08-25 17:20:59 +0500

    remove header on certificate view

---
+++ main/templatetags/startswith.py
@@ -0,0 +1,15 @@
+"""Module for custom string utility template tags and filters"""
+from django import template
+
+register = template.Library()
+
+
+@register.filter("startswith")
+def startswith(text, starts):
+    """Filter to check if a string starts with a specific format"""
+    if isinstance(text, str):
+        starts = starts.split(",")
+        for start in starts:
+            if text.startswith(start):
+                return True
+    return False
