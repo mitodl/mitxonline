4d812f1bc36b088885c18521203e227afe9e47a2
Collin Preston <collin_preston@yahoo.com>
2022-09-01 13:08:19 -0400

    Fix test

--- frontend/public/src/components/EnrolledItemCard_test.js
+++ frontend/public/src/components/EnrolledItemCard_test.js
@@ -143,7 +143,7 @@ describe("EnrolledItemCard", () => {
       userEnrollment["approved_flexible_price_exists"] = approvedFlexiblePrice
       enrollmentCardProps.enrollment = userEnrollment
       const inner = await renderedCard()
-      const extraLinks = inner.find(".enrollment-extra-links").at(1)
+      const extraLinks = inner.find(".enrollment-extra-links").at(0)
       if (approvedFlexiblePrice) {
         const text = extraLinks.find("a").at(0)
         assert.isFalse(text.exists())
