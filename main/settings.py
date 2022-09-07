39008e7f15dbe63acde8cf926bdc539a0d8c06a4
Anna Gavrilman <annagav@mit.edu>
2022-09-07 16:42:10 +0200

    undo

--- main/settings.py
+++ main/settings.py
@@ -804,7 +804,7 @@ CELERY_BEAT_SCHEDULE = {
     "sync-courseruns-data": {
         "task": "courses.tasks.sync_courseruns_data",
         "schedule": crontab(
-            minute="*",
+            minute=0,
             hour=CRON_COURSERUN_SYNC_HOURS,
             day_of_week=CRON_COURSERUN_SYNC_DAYS,
             day_of_month="*",
