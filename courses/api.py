52b06d758a6c19b352796499d6a6d5a22ac1b643
Collin Preston <collin_preston@yahoo.com>
2022-10-18 08:44:51 -0400

    Revert "Add test and extra documentation"
    This reverts commit 867fecb4d98cd1ca4be4463ed11bf3b623f8b157.

--- courses/api.py
+++ courses/api.py
@@ -229,7 +229,7 @@ def create_run_enrollments(
 
     Returns:
         (list of CourseRunEnrollment, bool): A list of enrollment objects that were successfully
-            created in mitxonline, paired with a boolean indicating whether or not edX enrollment API call was successful
+            created, paired with a boolean indicating whether or not edX enrollment was successful
             for all of the given course runs
     """
     successful_enrollments = []
