133e0a47ee70e1e48bcad1c2ba6a007cb73d77d3
James Kachel <jkachel@mit.edu>
2022-10-19 09:43:47 -0500

    Changes staff dashboard to use Django sessions rather than OAuth2

--- frontend/staff-dashboard/src/hooks/useAuthProvider.ts
+++ frontend/staff-dashboard/src/hooks/useAuthProvider.ts
@@ -1,36 +1,86 @@
 import { AuthProvider } from "@pankod/refine-core";
-import { useAuth } from "react-oidc-context";
+import axios from "axios";
 
 export function useAuthProvider(): AuthProvider {
-  const auth = useAuth()
+  const http = axios.create({
+    baseURL: DATASOURCES_CONFIG.mitxOnline
+  });
+  const _ = require("lodash");
+
+  const getProfile = async () => {
+    const profile = await http.get('users/me');
+
+    return Promise.resolve(profile.data);
+  }
+
   return {
     login: async () => {
-      if (!auth.isAuthenticated && !auth.activeNavigator && !auth.isLoading) {
-        let result = await auth.signinPopup()
-        if (result && result.profile["is_staff"] === true) {
-          return Promise.resolve();
-        }
-      }
-      return Promise.reject();
+      const profile = await getProfile();
 
+      if (_.get(profile, "data.is_staff") === true) {
+        localStorage.setItem('mitx-online-staff-profile', JSON.stringify(profile.data));
+        return Promise.resolve();
+      }
+
+      localStorage.removeItem("mitx-online-staff-profile");
+
+      return Promise.reject({
+        name: "Not Authenticated",
+        message: "Your account doesn't have permission to view this page."
+      });
     },
     logout: async () => {
-      await auth.removeUser();
+      localStorage.removeItem("mitx-online-staff-profile");
+      const logoutPath = (new URL(DATASOURCES_CONFIG.mitxOnline)).origin + "/logout/";
+      window.location.href = logoutPath;
       return Promise.resolve();
     },
     checkError: async () => {
       return Promise.resolve();
     },
     checkAuth: async () => {
-      let _ = require("lodash");
-      if (auth.isAuthenticated && auth.user && _.get(auth.user, "profile.is_staff") === true) {
+      const profile = await http.get('users/me');
+      
+      if (
+        _.get(profile, "data.is_superuser") === true ||
+        _.get(profile, "data.is_staff") === true
+      ) {
+        localStorage.setItem('mitx-online-staff-profile', JSON.stringify(profile.data));
         return Promise.resolve();
       }
-      return Promise.reject();
+      return Promise.reject({
+        redirectPath: (new URL(DATASOURCES_CONFIG.mitxOnline)).origin + "/signin"
+      });
     },
-    getPermissions: () => Promise.resolve(),
+    getPermissions: async () => {
+      /* 
+      For MITx Online currently, the only permissions we care about are the 
+      'is_staff' and 'is_superuser' flags. 
+      
+      Future enhancement: The current profile API returns a list of all user 
+      permissions (as in, from the default Django permissions code), so the app
+      could use this to determine permissions outside of those two flags. (This
+      data is visible here.)
+      */
+      let profile = localStorage.getItem("mitx-online-staff-profile");
+
+      if (profile) {
+        profile = JSON.parse(profile);
+
+        if (_.get(profile, "is_superuser")) {
+          return Promise.resolve(["superuser"]);
+        }
+
+        if (_.get(profile, "is_staff")) {
+          return Promise.resolve(["staff"]);
+        }
+      }
+
+      return Promise.resolve([]);
+    },
     getUserIdentity: async () => {
-      return auth.user ? Promise.resolve(auth.user) : Promise.reject()
+      let profile = localStorage.getItem("mitx-online-staff-profile");
+      return profile ? Promise.resolve(JSON.parse(profile)) : Promise.reject()
     },
   };
 };
--- frontend/staff-dashboard/src/pages/login.tsx
+++ frontend/staff-dashboard/src/pages/login.tsx
@@ -1,33 +1,8 @@
-
-import { useLogin, useTitle } from "@pankod/refine-core";
-import { AntdLayout, Button, Icons, Space, Typography } from "@pankod/refine-antd";
-
-const { LoginOutlined } = Icons;
-
-
 export default function LoginPage() {
-    const Title = useTitle();
-    const { mutate: login, isLoading } = useLogin();
-    return (
-        <AntdLayout>
-            <div style={{ height: "100vh", display: "flex" }}>
-                <div style={{ maxWidth: "300px", margin: "auto", textAlign: "center" }}>
-                    {Title && <Title collapsed={false} />}
-                    <Space/>
-                    <Typography.Title>MITx Online<br/>Staff Dashboard</Typography.Title>
-                    <Space/>
-                    <Button
-                        type="primary"
-                        size="large"
-                        block
-                        icon={<LoginOutlined />}
-                        loading={isLoading}
-                        onClick={() => login({})}
-                    >
-                        Sign in
-                    </Button>
-                </div>
-            </div>
-        </AntdLayout>
-    );
+    const sign_in_url = (new URL(DATASOURCES_CONFIG.mitxOnline)).origin + "/signin/?next=/staff-dashboard/";
+
+    document.location = sign_in_url;
+    
+    return (<>Please wait while we redirect you to the sign in page...</>);
+    
 };
