3c2096ef1bc8b73e3314144c0df330c480dd85d7
Collin Preston <collinpreston@users.noreply.github.com>
2022-11-22 09:33:04 -0500

    1207 accessibility more dates popup on course pages lacks keyboard controls (#1230)
    * Fix: hold for testing later
    
    * jame's test
    
    * Fix: handle tabbing into popover
    
    This allows a user to press tab and the enter button in order to access the more dates options on a product page.
    
    * Fix: Format
    
    * Fix: make More Dates button bolder

--- frontend/public/src/entry/django.js
+++ frontend/public/src/entry/django.js
@@ -5,10 +5,21 @@ import "video.js"
 import "videojs-youtube/dist/Youtube"
 import $ from "jquery"
 $(".dates-tooltip").popover({
+  sanitize: false,
   template:
     '<div class="popover" role="tooltip">' +
     '<div class="arrow"></div>' +
     '<div class="popover-header py-2 px-0 mx-5"></div>' +
     '<div class="popover-body"></div>' +
     "</div>"
+})
+$(".dates-tooltip").on("shown.bs.popover", () => {
+  $(".date-link").attr("tabindex", 0)
+  $(".date-link")
+    .first()
+    .focus()
+  $(".date-link").on("click", () => {
+    $(".dates-tooltip").popover("hide")
+    $(".dates-tooltip").focus()
+  })
 })
