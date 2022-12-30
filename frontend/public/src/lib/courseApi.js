5923ec1cd523221c1e8457db22d96c40088e04a0
Arslan <arslan.ashraf@arbisoft.com>
2022-12-30 15:39:38 +0500

    fix: course passed status based on course date and pace

--- frontend/public/src/lib/courseApi.js
+++ frontend/public/src/lib/courseApi.js
@@ -108,13 +108,22 @@ export const isFinancialAssistanceAvailable = (run: CourseRunDetail) => {
 export const enrollmentHasPassingGrade = (enrollment: RunEnrollment) => {
   if (enrollment.grades && enrollment.grades.length > 0) {
     for (let i = 0; i < enrollment.grades.length; i++) {
-      if (enrollment.grades[i].passed) {
+      if (enrollment.grades[i].passed && checkPassedDates(enrollment.run)) {
         return true
       }
     }
   }
 
   return false
+}
+
+const checkPassedDates = (run: CourseRunDetail) => {
+  if (run.is_self_paced) {
+    return true
+  } else {
+    const now = moment()
+    return (notNil(run.end_date) && moment(run.end_date).isBefore(now) && notNil(run.certificate_available_date) && moment(run.certificate_available_date).isBefore(now))
+  }
 }
 
 const isNodeCompleted = (
