d42ca0a0be25d062f4be49a44857bd8f81cc4344
Arslan <arslan.ashraf@arbisoft.com>
2023-01-25 18:25:01 +0500

    add filter for flexible pricing

--- ecommerce/admin.py
+++ ecommerce/admin.py
@@ -104,7 +104,7 @@ class DiscountAdmin(admin.ModelAdmin):
     model = Discount
     search_fields = ["discount_type", "redemption_type", "discount_code"]
     list_display = ["id", "discount_code", "discount_type", "amount", "redemption_type"]
-    list_filter = ["discount_type", "redemption_type"]
+    list_filter = ["discount_type", "redemption_type", "for_flexible_pricing"]
 
 
 @admin.register(DiscountProduct)
