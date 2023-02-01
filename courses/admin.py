3b6bef9390af0a69dabc07f3f7daa7faeb25136e
Asad Ali <asad.ali@arbisoft.com>
2023-02-01 19:20:11 +0500

    use getattr

--- courses/admin.py
+++ courses/admin.py
@@ -73,7 +73,7 @@ class CourseAdmin(admin.ModelAdmin):
         """
         Adds `title` as readonly field while editing an existing object.
         """
-        if obj and hasattr(obj, "page"):
+        if getattr(obj, "page", None):
             return self.readonly_fields + ("title",)
         return self.readonly_fields
 
@@ -81,7 +81,7 @@ class CourseAdmin(admin.ModelAdmin):
         """
         Adds help text for `title` field while editing an existing object.
         """
-        if obj and hasattr(obj, "page"):
+        if getattr(obj, "page", None):
             product_page_edit_url = (
                 f"{reverse('wagtailadmin_home')}/pages/{obj.page.id}/edit"
             )
