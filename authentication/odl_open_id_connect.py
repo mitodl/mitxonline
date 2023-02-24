282bdb95ac59d5ea9af3257fa1c7e79e218e6ab0
Collin Preston <collin_preston@yahoo.com>
2023-02-24 16:46:14 -0500

    Format open ID connect wrapper

--- authentication/odl_open_id_connect.py
+++ authentication/odl_open_id_connect.py
@@ -2,8 +2,9 @@ from social_core.backends.open_id_connect import OpenIdConnectAuth
 
 
 class OdlOpenIdConnectAuth(OpenIdConnectAuth):
-    ''' 
-        Custom wrapper class for adding additional functionality to the
-        OpenIdConnectAuth child class.
-    '''
+    """
+    Custom wrapper class for adding additional functionality to the
+    OpenIdConnectAuth child class.
+    """
+
     name = "odl-oidc"
