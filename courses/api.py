bbf57cf32e6597db2364c92b78bf1eefee9594ab
Collin Preston <collin_preston@yahoo.com>
2023-03-08 06:37:43 -0500

    format

--- courses/api.py
+++ courses/api.py
@@ -73,10 +73,7 @@ def get_user_relevant_course_run_qset(
     Returns a QuerySet of relevant course runs
     """
     now = now or now_in_utc()
-    run_qset = (
-        course.courseruns.exclude(start_date=None)
-        .exclude(enrollment_start=None)
-    )
+    run_qset = course.courseruns.exclude(start_date=None).exclude(enrollment_start=None)
     if user and user.is_authenticated:
         user_enrollments = Count(
             "enrollments",
