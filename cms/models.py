22f32edb66578de9adcef485edbb2d2027ab5c69
James Kachel <jkachel@mit.edu>
2023-07-26 11:16:27 -0500

    something merged oddly for some reason

--- cms/models.py
+++ cms/models.py
@@ -823,6 +823,7 @@ class ProductPage(Page):
         FieldPanel("feature_image"),
         FieldPanel("faculty_section_title"),
         FieldPanel("faculty_members"),
+        InlinePanel("linked_faculty_members", label="Faculty Members"),
         FieldPanel("video_url"),
     ]
 
@@ -1548,3 +1549,21 @@ class InstructorIndexPage(InstructorObjectIndexPage):
     """
 
     slug = INSTRUCTOR_INDEX_SLUG
+
+
+class InstructorPageLink(models.Model):
+    page = ParentalKey(
+        ProductPage, on_delete=models.CASCADE, related_name="linked_faculty_members"
+    )
+
+    faculty_member_page = models.ForeignKey(
+        "wagtailcore.Page",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    panels = [
+        PageChooserPanel("faculty_member_page", "cms.InstructorPage"),
+    ]
