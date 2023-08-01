ee3a1c112894d4a48e1dbe59f5f1658427bdb2ce
Collin Preston <collin_preston@yahoo.com>
2023-08-01 11:38:02 -0400

    formatting

--- frontend/public/src/containers/pages/CatalogPage.js
+++ frontend/public/src/containers/pages/CatalogPage.js
@@ -40,14 +40,16 @@ export class CatalogPage extends React.Component<Props> {
   }
 
   componentDidUpdate = () => {
-    const {
-      coursesIsLoading,
-    } = this.props
+    const { coursesIsLoading } = this.props
     if (!coursesIsLoading && !this.state.filterCoursesCalled) {
       this.setState({ filterCoursesCalled: true })
-      const courses = this.filteredCoursesBasedOnCourseRunCriteria(this.state.selectedTopic)
+      const courses = this.filteredCoursesBasedOnCourseRunCriteria(
+        this.state.selectedTopic
+      )
       this.setState({ filteredCourses: courses })
-      this.setState({ courseTopics: this.collectCourseTopicsFromCourses(courses) })
+      this.setState({
+        courseTopics: this.collectCourseTopicsFromCourses(courses)
+      })
     }
   }
 
@@ -62,10 +64,17 @@ export class CatalogPage extends React.Component<Props> {
 
   changeSelectedTopic = (selectedTopic: string) => {
     this.setState({ selectedTopic: selectedTopic })
-    this.setState({ filteredCourses: this.filteredCoursesBasedOnCourseRunCriteria(selectedTopic) })
+    this.setState({
+      filteredCourses: this.filteredCoursesBasedOnCourseRunCriteria(
+        selectedTopic
+      )
+    })
   }
 
-  getFutureCourseRunClosestToToday(courseRunA: BaseCourseRun, courseRunB: BaseCourseRun) {
+  getFutureCourseRunClosestToToday(
+    courseRunA: BaseCourseRun,
+    courseRunB: BaseCourseRun
+  ) {
     if (courseRunA.start_date > courseRunB.start_date) {
       return -1
     }
@@ -77,11 +86,19 @@ export class CatalogPage extends React.Component<Props> {
   }
 
   renderCatalogCardTagForCourse(course: CourseDetailWithRuns) {
-    const nonSelfPacedCourseRuns = course.courseruns.filter(courseRun => !courseRun.is_self_paced)
+    const nonSelfPacedCourseRuns = course.courseruns.filter(
+      courseRun => !courseRun.is_self_paced
+    )
     if (nonSelfPacedCourseRuns.length > 0) {
-      const futureStartDateCourseRuns = nonSelfPacedCourseRuns.filter(courseRun => moment(courseRun.start_date).isAfter(moment()))
+      const futureStartDateCourseRuns = nonSelfPacedCourseRuns.filter(
+        courseRun => moment(courseRun.start_date).isAfter(moment())
+      )
       if (futureStartDateCourseRuns.length > 0) {
-        const startDate = parseDateString(futureStartDateCourseRuns.sort(this.getFutureCourseRunClosestToToday)[0].start_date)
+        const startDate = parseDateString(
+          futureStartDateCourseRuns.sort(
+            this.getFutureCourseRunClosestToToday
+          )[0].start_date
+        )
         return `Start Date: ${formatPrettyDate(startDate)}`
       } else {
         return "Start Anytime"
@@ -92,24 +109,25 @@ export class CatalogPage extends React.Component<Props> {
   }
 
   validateCoursesCourseRuns(courseRuns: Array<BaseCourseRun>) {
-    return courseRuns.filter(courseRun =>
-      courseRun.live &&
-      courseRun.start_date &&
-      moment(courseRun?.enrollment_start).isBefore(moment()) &&
-      (!courseRun.enrollment_end || moment(courseRun.enrollment_end).isAfter(moment())))
+    return courseRuns.filter(
+      courseRun =>
+        courseRun.live &&
+        courseRun.start_date &&
+        moment(courseRun?.enrollment_start).isBefore(moment()) &&
+        (!courseRun.enrollment_end ||
+          moment(courseRun.enrollment_end).isAfter(moment()))
+    )
   }
 
   filteredCoursesBasedOnCourseRunCriteria(selectedTopic: string) {
-    const {
-      courses,
-    } = this.props
-    return courses.filter(course =>
-      (
-        (selectedTopic === ALL_DEPARTMENTS || course.topics.map(topic => topic.name).includes(selectedTopic)) &&
+    const { courses } = this.props
+    return courses.filter(
+      course =>
+        (selectedTopic === ALL_DEPARTMENTS ||
+          course.topics.map(topic => topic.name).includes(selectedTopic)) &&
         course.page.live === true &&
         course.courseruns.length > 0 &&
         this.validateCoursesCourseRuns(course.courseruns).length > 0
-      )
     )
   }
 
@@ -117,11 +135,7 @@ export class CatalogPage extends React.Component<Props> {
    * Returns the number of courseRuns or programs based on the selected catalog tab.
    */
   renderNumberOfCatalogItems() {
-    const {
-      programs,
-      coursesIsLoading,
-      programsIsLoading
-    } = this.props
+    const { programs, coursesIsLoading, programsIsLoading } = this.props
     if (this.state.tabSelected === "courses" && !coursesIsLoading) {
       return this.state.filteredCourses.length
     } else if (this.state.tabSelected === "programs" && !programsIsLoading) {
@@ -137,9 +151,7 @@ export class CatalogPage extends React.Component<Props> {
     return (
       <a href={course.page.page_url} key={course.id}>
         <div className="col catalog-item">
-          {
-            <img src={course?.page?.feature_image_src} alt="" />
-          }
+          {<img src={course?.page?.feature_image_src} alt="" />}
           <div className="catalog-item-description">
             <div className="start-date-description">
               {this.renderCatalogCardTagForCourse(course)}
@@ -160,9 +172,7 @@ export class CatalogPage extends React.Component<Props> {
       <a href={program.page.page_url} key={program.id}>
         <div className="col catalog-item">
           <div className="program-image-and-badge">
-            {
-              <img src={program?.page?.feature_image_src} alt="" />
-            }
+            {<img src={program?.page?.feature_image_src} alt="" />}
             <div className="program-type-badge">{program.program_type}</div>
           </div>
           <div className="catalog-item-description">
@@ -199,12 +209,15 @@ export class CatalogPage extends React.Component<Props> {
    * Renders the entire catalog of course or program cards based on the catalog tab selected.
    */
   renderCatalog() {
-    const {
-      programs,
-      programsIsLoading
-    } = this.props
-    if (this.state.tabSelected === "courses" && this.state.filteredCourses.length > 0) {
-      return this.renderCatalogRows(this.state.filteredCourses, this.renderCourseCatalogCard.bind(this))
+    const { programs, programsIsLoading } = this.props
+    if (
+      this.state.tabSelected === "courses" &&
+      this.state.filteredCourses.length > 0
+    ) {
+      return this.renderCatalogRows(
+        this.state.filteredCourses,
+        this.renderCourseCatalogCard.bind(this)
+      )
     } else if (this.state.tabSelected === "programs" && !programsIsLoading) {
       return this.renderCatalogRows(programs, this.renderProgramCatalogCard)
     }
@@ -214,7 +227,14 @@ export class CatalogPage extends React.Component<Props> {
     const departmentSideBarListItems = []
     this.state.courseTopics.forEach(courseTopic =>
       departmentSideBarListItems.push(
-        <li className={this.state.selectedTopic === courseTopic ? "department-selected-link" : "department-link"} key={courseTopic}>
+        <li
+          className={
+            this.state.selectedTopic === courseTopic
+              ? "department-selected-link"
+              : "department-link"
+          }
+          key={courseTopic}
+        >
           <button onClick={() => this.changeSelectedTopic(courseTopic)}>
             {courseTopic}
           </button>
@@ -223,9 +243,7 @@ export class CatalogPage extends React.Component<Props> {
     )
     return (
       <div id="department-sidebar">
-        <ul id="department-sidebar-link-list">
-          {departmentSideBarListItems}
-        </ul>
+        <ul id="department-sidebar-link-list">{departmentSideBarListItems}</ul>
       </div>
     )
   }
@@ -255,7 +273,9 @@ export class CatalogPage extends React.Component<Props> {
                             : "unselected-tab"
                         }
                       >
-                        <button onClick={() => this.changeSelectedTab("courses")}>
+                        <button
+                          onClick={() => this.changeSelectedTab("courses")}
+                        >
                           Courses
                         </button>
                       </div>
@@ -266,7 +286,9 @@ export class CatalogPage extends React.Component<Props> {
                             : "unselected-tab"
                         }
                       >
-                        <button onClick={() => this.changeSelectedTab("programs")}>
+                        <button
+                          onClick={() => this.changeSelectedTab("programs")}
+                        >
                           Programs
                         </button>
                       </div>
@@ -284,7 +306,8 @@ export class CatalogPage extends React.Component<Props> {
                     >
                       <div>
                         {/* Could add logic to display only "course" if only 1 course is showing. */}
-                        {this.renderNumberOfCatalogItems()} {this.state.tabSelected}
+                        {this.renderNumberOfCatalogItems()}{" "}
+                        {this.state.tabSelected}
                       </div>
                     </CSSTransition>
                   </TransitionGroup>
@@ -298,9 +321,7 @@ export class CatalogPage extends React.Component<Props> {
                   timeout={300}
                   classNames="messageout"
                 >
-                  <div>
-                    {this.renderCatalog()}
-                  </div>
+                  <div>{this.renderCatalog()}</div>
                 </CSSTransition>
               </TransitionGroup>
             </div>
@@ -314,13 +335,9 @@ export class CatalogPage extends React.Component<Props> {
 const mapPropsToConfig = () => [coursesQuery(), programsQuery()]
 
 const mapStateToProps = createStructuredSelector({
-  courses:          coursesSelector,
-  programs:         programsSelector,
-  coursesIsLoading: pathOr(true, [
-    "queries",
-    coursesQueryKey,
-    "isPending"
-  ]),
+  courses:           coursesSelector,
+  programs:          programsSelector,
+  coursesIsLoading:  pathOr(true, ["queries", coursesQueryKey, "isPending"]),
   programsIsLoading: pathOr(true, ["queries", programsQueryKey, "isPending"])
 })
 
