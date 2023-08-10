352b8e3075166c63ea9ad2c13bdda5e912ebd837
Doof <mitx-devops@mit.edu>
2023-08-10 09:57:14 +0000

    Release 0.70.6

--- main/settings.py
+++ main/settings.py
@@ -28,7 +28,7 @@ from redbeat import RedBeatScheduler
 from main.celery_utils import OffsettingSchedule
 from main.sentry import init_sentry
 
-VERSION = "0.70.5"
+VERSION = "0.70.6"
 
 log = logging.getLogger()
 
