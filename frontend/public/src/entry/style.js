1b298d1fd4f6f206f8d90de779735fb3909f200d
jenniw <jenniw@mit.edu>
2023-08-15 14:09:14 -0400

    something in setting is busted

--- frontend/public/src/entry/style.js
+++ frontend/public/src/entry/style.js
@@ -1,1 +1,9 @@
-import "../../scss/layout.scss"
+import { checkFeatureFlag } from "../lib/util"
+
+if (checkFeatureFlag("mitxonline-new-product-page")) {
+  console.log("Success")
+  import("../../scss/layout-v2.scss")
+} else {
+  console.log("not success")
+  import("../../scss/layout.scss")
+}
