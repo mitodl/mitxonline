b72c8df6fc28a1076189e80d9616aaede12fb236
jenniw <jenniw@mit.edu>
2023-08-16 09:11:45 -0400

    pushing the css & refactoring & moving component out in a moment

--- frontend/public/src/entry/style.js
+++ frontend/public/src/entry/style.js
@@ -1,9 +1,7 @@
 import { checkFeatureFlag } from "../lib/util"
 
 if (checkFeatureFlag("mitxonline-new-product-page")) {
-  console.log("Success")
   import("../../scss/layout-v2.scss")
 } else {
-  console.log("not success")
   import("../../scss/layout.scss")
 }
