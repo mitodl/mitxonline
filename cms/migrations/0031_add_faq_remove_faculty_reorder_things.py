6775405dcd4bdf9dc8dd8962dd718b11540718d7
James Kachel <jkachel@mit.edu>
2023-08-18 09:40:05 -0500

    removing merge migration

--- cms/migrations/0031_add_faq_remove_faculty_reorder_things.py
+++ cms/migrations/0031_add_faq_remove_faculty_reorder_things.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
     dependencies = [
-        ("cms", "0029_add_linked_instructors_to_product_pages"),
+        ("cms", "0030_move_instructor_short_bios_to_long"),
     ]
 
     operations = [
