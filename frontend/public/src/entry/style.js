07978fa6712eabf07530e6de8139a9fc649b5df5
jenniw <jenniw@mit.edu>
2023-08-24 09:45:19 -0400

    import home_page_hero.scss file on flag

--- frontend/public/src/entry/style.js
+++ frontend/public/src/entry/style.js
@@ -4,3 +4,6 @@ import("../../scss/layout.scss")
 if (checkFeatureFlag("mitxonline-new-featured-carousel")) {
   import("../../scss/featured-product-cards.scss")
 }
+if (checkFeatureFlag("mitxonline-new-featured-hero")) {
+  import("../../scss/home-page-hero.scss")
+}
