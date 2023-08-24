bdac6415a934080b8a529550b6ede343b877e899
jenniw <jenniw@mit.edu>
2023-08-24 09:43:32 -0400

    wrong flag name

--- frontend/public/src/entry/style.js
+++ frontend/public/src/entry/style.js
@@ -1,6 +1,6 @@
 import { checkFeatureFlag } from "../lib/util"
 import("../../scss/layout.scss")
 
-if (checkFeatureFlag("mitxonline-new-product-page")) {
+if (checkFeatureFlag("mitxonline-new-featured-carousel")) {
   import("../../scss/featured-product-cards.scss")
 }
