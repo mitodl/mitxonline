726a64faed88eb080f571344599546f997476a2a
jenniw <JenniWhitman@users.noreply.github.com>
2023-08-29 14:14:07 -0400

    Moving home-page files, pulling out unnecessary imports from styles.js and meta-product-page (#1838)

--- frontend/public/src/entry/style.js
+++ frontend/public/src/entry/style.js
@@ -1,17 +1,5 @@
 import { checkFeatureFlag } from "../lib/util"
-import("../../scss/layout.scss")
-
-if (checkFeatureFlag("mitxonline-new-featured-carousel")) {
-  import("../../scss/featured-product-cards.scss")
-}
-
-if (checkFeatureFlag("mitxonline-new-featured-hero")) {
-  import("../../scss/home-page-hero.scss")
-}
-
-if (checkFeatureFlag("mitxonline-new-home-page-video-component")) {
-  import("../../scss/home-page-video-component.scss")
-}
+import "../../scss/layout.scss"
 
 if (checkFeatureFlag("mitxonline-new-product-page")) {
   import("../../scss/meta-product-page.scss")
