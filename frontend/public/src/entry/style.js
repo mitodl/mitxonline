a68f477776c3b7795a23f8fc2b56703122968f1d
jenniw <jenniw@mit.edu>
2023-08-29 11:57:01 -0400

    playing with scss

--- frontend/public/src/entry/style.js
+++ frontend/public/src/entry/style.js
@@ -1,6 +1,6 @@
 import { checkFeatureFlag } from "../lib/util"
 import "../../scss/layout.scss"
 
-// if (checkFeatureFlag("mitxonline-new-product-page")) {
-//   import("../../scss/meta-product-page.scss")
-// }
+if (checkFeatureFlag("mitxonline-new-product-page")) {
+  import("../../scss/meta-product-page.scss")
+}
