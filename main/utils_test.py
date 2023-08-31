a5ead0a6e8831c99a6ad3ad848c69d1ed5c1a76c
Collin Preston <collin_preston@yahoo.com>
2023-08-31 15:45:45 -0400

    Fix test

--- main/utils_test.py
+++ main/utils_test.py
@@ -46,6 +46,7 @@ def test_get_js_settings(settings, rf):
         "recaptchaKey": settings.RECAPTCHA_SITE_KEY,
         "support_email": settings.EMAIL_SUPPORT,
         "site_name": settings.SITE_NAME,
+        "features": {},
         "posthog_api_token": settings.POSTHOG_API_TOKEN,
         "posthog_api_host": settings.POSTHOG_API_HOST,
     }
