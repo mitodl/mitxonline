4861916c16826671ac8e4610929de5baf8cbf606
James Kachel <jkachel@mit.edu>
2023-09-07 09:02:40 -0500

    Updates the course about page CSS (#1865)
    Works better with feature flagged content.

--- frontend/public/src/entry/style.js
+++ frontend/public/src/entry/style.js
@@ -1,6 +1,1 @@
-import { checkFeatureFlag } from "../lib/util"
 import "../../scss/layout.scss"
-
-if (checkFeatureFlag("mitxonline-new-product-page")) {
-  import("../../scss/meta-product-page.scss")
-}
