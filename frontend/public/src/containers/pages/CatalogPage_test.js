f49e9c6cfc5fe5b85218b41fca795192def17703
Collin Preston <collinpreston@users.noreply.github.com>
2023-09-12 11:33:09 -0400

    Department REST API (#1877)
    * This works but is just POC
    
    * Add department serializer and prefetch
    
    * improvement for courses API
    
    * program improvements
    
    * Fix course run serializer, migration, enrollment api
    
    * Add live filter to front end API call
    
    * Fix tests, format, add filtering to front end query
    
    * select related course on course_run API
    
    * Javascript formatting
    
    * Fix serializers
    
    * Fix Course serializer
    
    * Format
    
    * py test passing
    
    * Format and repair tests
    
    * Clean up department serializer
    
    * hold
    
    * Departments api, catalog update
    
    * Clean up
    
    * Repair javascript tests
    
    * Format serializer
    
    * Add queryset

--- frontend/public/src/containers/pages/CatalogPage_test.js
+++ frontend/public/src/containers/pages/CatalogPage_test.js
@@ -279,6 +279,10 @@ describe("CatalogPage", function() {
           programs: {
             isPending: false,
             status:    200
+          },
+          departments: {
+            isPending: false,
+            status:    200
           }
         },
         entities: {
@@ -287,7 +291,19 @@ describe("CatalogPage", function() {
           },
           programs: {
             results: programs
-          }
+          },
+          departments: [
+            {
+              name:     "History",
+              courses:  0,
+              programs: 5
+            },
+            {
+              name:     "Science",
+              courses:  1,
+              programs: 0
+            }
+          ]
         }
       },
       {}
@@ -305,34 +321,53 @@ describe("CatalogPage", function() {
     expect(inner.instance().renderNumberOfCatalogItems()).equals(5)
   })
 
-  it("renders catalog department filter for courses and programs without duplicates", async () => {
-    const course1 = JSON.parse(JSON.stringify(displayedCourse))
-    course1.departments = [{ name: "Math" }]
-    const course2 = JSON.parse(JSON.stringify(displayedCourse))
-    course2.departments = [{ name: "Math" }, { name: "Science" }]
-    const course3 = JSON.parse(JSON.stringify(displayedCourse))
-    course3.departments = [{ name: "Math" }, { name: "History" }]
-    courses = [course1, course2, course3]
-
-    const program1 = JSON.parse(JSON.stringify(displayedProgram))
-    program1.departments = [{ name: "Computer Science" }, { name: "Physics" }]
-    const program2 = JSON.parse(JSON.stringify(displayedProgram))
-    program2.departments = []
-    const program3 = JSON.parse(JSON.stringify(displayedProgram))
-    program3.departments = [{ name: "Physics" }]
-    programs = [program1, program2, program3]
-    const { inner } = await renderPage()
-    let collectedDepartments = inner
-      .instance()
-      .collectDepartmentsFromCatalogItems(courses)
-    expect(JSON.stringify(collectedDepartments)).equals(
-      JSON.stringify(["All Departments", "Math", "Science", "History"])
+  it("renders catalog department filter for courses and programs tabs", async () => {
+    const { inner } = await renderPage(
+      {
+        queries: {
+          departments: {
+            isPending: false,
+            status:    200
+          }
+        },
+        entities: {
+          departments: [
+            {
+              name:     "department1",
+              courses:  1,
+              programs: 0
+            },
+            {
+              name:     "department2",
+              courses:  1,
+              programs: 1
+            },
+            {
+              name:     "department3",
+              courses:  0,
+              programs: 1
+            },
+            {
+              name:     "department4",
+              courses:  0,
+              programs: 0
+            }
+          ]
+        }
+      },
+      {}
     )
-    collectedDepartments = inner
+    let filteredDepartments = inner
       .instance()
-      .collectDepartmentsFromCatalogItems(programs)
-    expect(JSON.stringify(collectedDepartments)).equals(
-      JSON.stringify(["All Departments", "Computer Science", "Physics"])
+      .filterDepartmentsByTabName("courses")
+    expect(JSON.stringify(filteredDepartments)).equals(
+      JSON.stringify(["All Departments", "department1", "department2"])
+    )
+    filteredDepartments = inner
+      .instance()
+      .filterDepartmentsByTabName("programs")
+    expect(JSON.stringify(filteredDepartments)).equals(
+      JSON.stringify(["All Departments", "department2", "department3"])
     )
   })
 
@@ -622,6 +657,10 @@ describe("CatalogPage", function() {
           programs: {
             isPending: false,
             status:    200
+          },
+          departments: {
+            isPending: false,
+            status:    200
           }
         },
         entities: {
@@ -630,7 +669,24 @@ describe("CatalogPage", function() {
           },
           programs: {
             results: [displayedProgram]
-          }
+          },
+          departments: [
+            {
+              name:     "History",
+              courses:  2,
+              programs: 1
+            },
+            {
+              name:     "Math",
+              courses:  3,
+              programs: 0
+            },
+            {
+              name:     "department4",
+              courses:  0,
+              programs: 0
+            }
+          ]
         }
       },
       {}
@@ -867,6 +923,10 @@ describe("CatalogPage", function() {
           programs: {
             isPending: false,
             status:    200
+          },
+          departments: {
+            isPending: false,
+            status:    200
           }
         },
         entities: {
@@ -877,7 +937,14 @@ describe("CatalogPage", function() {
           programs: {
             results: programs,
             next:    "http://fake.com/api/courses/?page=2"
-          }
+          },
+          departments: [
+            {
+              name:     "History",
+              courses:  1,
+              programs: 1
+            }
+          ]
         }
       },
       {}
