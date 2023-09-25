562be70e8945858d98a6a1a4997cd74d6f1de3f7
jenniw <jenniw@mit.edu>
2023-09-25 12:29:34 -0400

    fmt

--- frontend/public/src/components/CourseInfoBox.js
+++ frontend/public/src/components/CourseInfoBox.js
@@ -98,8 +98,11 @@ export default class CourseInfoBox extends React.PureComponent<CourseInfoBoxProp
                     </>
                   ) : null}
                   <div>
-                    <a target="_blank" rel="noreferrer"
-                      href="https://mitxonline.zendesk.com/hc/en-us/articles/16928404973979-Does-MITx-Online-offer-free-certificates-">
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href="https://mitxonline.zendesk.com/hc/en-us/articles/16928404973979-Does-MITx-Online-offer-free-certificates-"
+                    >
                       What's the certificate track?
                     </a>
                   </div>
