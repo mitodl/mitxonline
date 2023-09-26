3dd51931d254d70f8d0c01198ee85b247f150149
jenniw <jenniw@mit.edu>
2023-09-26 11:49:09 -0400

    just styling

--- frontend/public/src/components/CourseInfoBox.js
+++ frontend/public/src/components/CourseInfoBox.js
@@ -29,14 +29,20 @@ export default class CourseInfoBox extends React.PureComponent<CourseInfoBoxProp
 
     const product = run && run.products.length > 0 && run.products[0]
 
+    const isArchived = moment().isAfter(run.end_date)
+
     const startDate =
-      run && !emptyOrNil(run.start_date) && !run.is_self_paced
+      run && !emptyOrNil(run.start_date) && !run.is_self_paced && !isArchived
         ? moment(new Date(run.start_date))
         : null
 
     return (
       <>
         <div className="enrollment-info-box componentized">
+          <div className="row d-flex align-self-stretch callout callout-warning">
+            <i className="material-symbols-outlined warning col-1">error</i>
+            <p className="col-11">This course is no longer active, but you can still enroll and access selected content.</p>
+          </div>
           <div className="row d-flex align-items-center">
             <div className="enrollment-info-icon">
               <img
@@ -45,7 +51,7 @@ export default class CourseInfoBox extends React.PureComponent<CourseInfoBoxProp
               />
             </div>
             <div className="enrollment-info-text">
-              {startDate ? formatPrettyDate(startDate) : "Start Anytime"}
+              {startDate ? formatPrettyDate(startDate) : isArchived ? "Course content available anytime" : "Start Anytime"}
             </div>
           </div>
           {course && course.page ? (
@@ -97,30 +103,29 @@ export default class CourseInfoBox extends React.PureComponent<CourseInfoBoxProp
                       </div>
                     </>
                   ) : null}
-                  <div>
-                    <a
-                      target="_blank"
-                      rel="noreferrer"
-                      href="https://mitxonline.zendesk.com/hc/en-us/articles/16928404973979-Does-MITx-Online-offer-free-certificates-"
-                    >
-                      What's the certificate track?
-                    </a>
-                  </div>
-                  {course.page.financial_assistance_form_url ? (
-                    <div>
-                      <a
-                        target="_blank"
-                        rel="noreferrer"
-                        href={course.page.financial_assistance_form_url}
-                      >
-                        Financial assistance available
-                      </a>
-                    </div>
-                  ) : null}
                 </>
               ) : (
                 "No certificate available."
               )}
+              <div>
+                <a
+                  target="_blank"
+                  rel="noreferrer"
+                  href="https://mitxonline.zendesk.com/hc/en-us/articles/16928404973979-Does-MITx-Online-offer-free-certificates-"
+                >
+                  What's the certificate track?
+                </a>
+              </div>
+              {course.page.financial_assistance_form_url ? (
+                <div>
+                  <a
+                    target="_blank"
+                    rel="noreferrer"
+                    href={course.page.financial_assistance_form_url}
+                  >
+                    Financial assistance available
+                  </a>
+                </div>) : null}
             </div>
           </div>
         </div>
