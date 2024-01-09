fd4163f4852ea35ecda4fe86c427063f65d6ed87
jenniw <jenniw@mit.edu>
2024-01-09 11:26:59 -0500

    updated components with links. need actual links.

--- frontend/public/src/components/CourseInfoBox.js
+++ frontend/public/src/components/CourseInfoBox.js
@@ -132,8 +132,24 @@ export default class CourseInfoBox extends React.PureComponent<CourseInfoBoxProp
               <div className="enrollment-info-text">
                 {course.page.length}
                 {run && run.is_self_paced ? (
-                  <span className="badge badge-pacing">SELF-PACED</span>
-                ) : null}
+                  <>
+                    <span className="badge badge-pacing">SELF-PACED</span>
+                    <a className="pacing-faq-link float-right" href="/">
+                      What's this?
+                    </a>
+                  </>
+                ) : (
+                  <>
+                    <span className="badge badge-pacing">INSTRUCTOR-PACED</span>
+                    <a
+                      className="pacing-faq-link float-right"
+                      href="https://google.com"
+                    >
+                      What's this?
+                    </a>
+                  </>
+                )}
+
                 {course.page.effort ? (
                   <>
                     <div className="enrollment-effort">
