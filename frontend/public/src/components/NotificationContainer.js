d50361249211dc93a9ae6624cbac8376d3761d63
Collin Preston <collin_preston@yahoo.com>
2024-01-16 15:36:42 -0500

    lint

--- frontend/public/src/components/NotificationContainer.js
+++ frontend/public/src/components/NotificationContainer.js
@@ -94,7 +94,7 @@ export class NotificationContainer extends React.Component<Props, State> {
   }
 }
 
-const mapStateToProps = (state) => ({
+const mapStateToProps = state => ({
   userNotifications: state.ui.userNotifications,
 })
 
