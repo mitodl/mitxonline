e5c0a77af63c0fabbc239b07cb42ac928cfdc2b7
Anna <anna@Jens-MacBook-Pro.local>
2024-01-18 13:56:47 -0500

    Show More link on product about page accessibility

--- cms/templatetags/expand.py
+++ cms/templatetags/expand.py
@@ -37,11 +37,10 @@ def expand(text):
         output = f'{pre}<p class="expand_here_container"><a href="#" class="expand_here_link" data-expand-body="{container_uuid}">Show More</a></p><div class="expand_here_body" id="exp{container_uuid}">{str(expand_here[0])}{post}</div>'
     elif len(soup.find_all(["p", "div"])) > 1:
         expand_here = soup.find_all(["p", "div"], limit=2)
-
         pre = str(expand_here[0])
         post = "".join([str(sib) for sib in expand_here[1].find_next_siblings()])
 
-        output = f'<!-- pre -->{pre}<!-- /pre --><div class="expand_here_body" id="exp{container_uuid}">{str(expand_here[1])}{post}</div><p class="expand_here_container"><a href="#" class="expand_here_link fade" data-expand-body="{container_uuid}">Show More</a></p>'
+        output = f'<!-- pre -->{pre}<!-- /pre --><p class="expand_here_container"><a href="#" class="expand_here_link fade" data-expand-body="{container_uuid}">Show More</a></p><div class="expand_here_body hide" id="exp{container_uuid}">{str(expand_here[1])}{post}</div>'
     elif len(text.split("\n\n")) > 1:
         (pre, post) = text.split("\n\n", maxsplit=1)
 
