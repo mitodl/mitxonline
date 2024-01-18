0115ae55ec2f64ef8c97d5749a729334b56e59b5
James Kachel <jkachel@mit.edu>
2024-01-18 14:55:27 -0600

    Fixed import for catalog view that doesn't exist anymore

--- main/urls.py
+++ main/urls.py
@@ -24,7 +24,7 @@ from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
 
 from cms.views import instructor_page
-from main.views import cms_signin_redirect_to_site_signin, index, refine, catalog
+from main.views import cms_signin_redirect_to_site_signin, index, refine
 
 handler500 = "main.views.handler500"
 handler404 = "main.views.handler404"
@@ -77,7 +77,7 @@ urlpatterns = [
     re_path(r"^orders/history/.*", index, name="order-history"),
     re_path(r"^orders/receipt/.*", index, name="order-receipt"),
     re_path(r"^records/.*", index, name="learner-records"),
-    re_path(r"^catalog/", catalog, name="catalog"),
+    re_path(r"^catalog/", index, name="catalog"),
     path("api/instructor/<int:id>/", instructor_page, name="cms_instructor_page"),
     # Wagtail
     re_path(
