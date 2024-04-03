7052b4cbc94839123e8be9a353cac70c7c999fc8
Collin Preston <collin_preston@yahoo.com>
2024-04-03 08:31:23 -0400

    Fix tests

--- main/utils.py
+++ main/utils.py
@@ -52,7 +52,7 @@ def get_js_settings(request: HttpRequest):
         "features": {},
         "posthog_api_token": settings.POSTHOG_API_TOKEN,
         "posthog_api_host": settings.POSTHOG_API_HOST,
-        "posthog_feature_flag_request_timeout_ms": settings.POSTHOG_REQUEST_TIMEOUT_MS,
+        "posthog_feature_flag_request_timeout_ms": settings.POSTHOG_FEATURE_FLAG_REQUEST_TIMEOUT_MS,
     }
 
 
