37aa9aa06fad18db357c7e19a4b1ee025c5a3ce8
jenniw <jenniw@mit.edu>
2024-06-03 13:53:16 -0400

    update request

--- main/views.py
+++ main/views.py
@@ -17,7 +17,6 @@ def get_base_context(request):  # noqa: ARG001
     Returns the template context key/values needed for the base template and all templates that extend it
     """
     context = {}
-
     if settings.GOOGLE_DOMAIN_VERIFICATION_TAG_VALUE:
         context["domain_verification_tag"] = (
             settings.GOOGLE_DOMAIN_VERIFICATION_TAG_VALUE
@@ -40,21 +39,21 @@ def refine(request, **kwargs):  # noqa: ARG001
     """
     The refine view for the staff dashboard
     """
-    return render(request, "refine.html", context=get_base_context())
+    return render(request, "refine.html", context=get_base_context(request))
 
 
 def handler404(request, exception):  # pylint: disable=unused-argument  # noqa: ARG001
     """404: NOT FOUND ERROR handler"""
     context = get_base_context()  # noqa: F841
     return HttpResponseNotFound(
-        render_to_string("404.html", request=request, context=get_base_context())
+        render_to_string("404.html", request=request, context=get_base_context(request))
     )
 
 
 def handler500(request):
     """500 INTERNAL SERVER ERROR handler"""
     return HttpResponseServerError(
-        render_to_string("500.html", request=request, context=get_base_context())
+        render_to_string("500.html", request=request, context=get_base_context(request))
     )
 
 
