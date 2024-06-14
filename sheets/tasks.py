ddbdcc8bb644b34a2956988931873c0f14f52258
Anna <annagav@mit.edu>
2024-06-14 17:20:56 -0400

    revoking scheduled tasks

--- sheets/tasks.py
+++ sheets/tasks.py
@@ -7,38 +7,19 @@ from main.celery import app
 
 log = logging.getLogger(__name__)
 
-LOCK_EXPIRE = 60 * 10  # Lock expires in 10 minutes
-
-@contextmanager
-def memcache_lock(lock_id, oid):
-    timeout_at = time.monotonic() + LOCK_EXPIRE - 3
-    # cache.add fails if the key already exists
-    status = cache.add(lock_id, oid, LOCK_EXPIRE)
-    try:
-        yield status
-    finally:
-        # memcache delete is very slow, but we have to use it to take
-        # advantage of using add() for atomic locking
-        if time.monotonic() < timeout_at and status:
-            # don't release the lock if we exceeded the timeout
-            # to lessen the chance of releasing an expired lock
-            # owned by someone else
-            # also don't release the lock if we didn't acquire it
-            cache.delete(lock_id)
 
 @app.task(bind=True)
-def process_google_sheets_requests():
+def process_google_sheets_requests(self):
     """
     Task to process refund and deferral requests from Google sheets
     """
-    feed_url_hexdigest = md5(feed_url).hexdigest()
-    lock_id = '{0}-lock-{1}'.format(self.name, feed_url_hexdigest)
-    logger.debug('Importing feed: %s', feed_url)
-    with memcache_lock(lock_id, self.app.oid) as acquired:
-        if acquired:
-            return Feed.objects.import_feed(feed_url).url
-    logger.debug(
-        'Feed %s is already being imported by another worker', feed_url)
+    # revoke all previously scheduled tasks
+    # log.info(self.name)
+    # log.info(app.signature(self.name))
+    query = app.events.state.tasks_by_type(self.name)
+    for uuid, task in query:
+        app.control.revoke(uuid)
+
     refund_request_handler = RefundRequestHandler()
     deferral_request_handler = DeferralRequestHandler()
     if refund_request_handler.is_configured():
