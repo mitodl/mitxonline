2b35be580a04d0e1b5f90fb25666b399d71d7bd4
Collin Preston <collinpreston@users.noreply.github.com>
2024-06-21 14:28:37 -0400

    Program enroll button text does not appear on about page (#2265)
    * Fix
    
    * format

--- frontend/public/src/components/ProgramProductDetailEnroll.js
+++ frontend/public/src/components/ProgramProductDetailEnroll.js
@@ -404,9 +404,7 @@ export class ProgramProductDetailEnroll extends React.Component<
             <>
               {enrollment ? (
                 <Fragment>
-                  <div
-                    className={`btn btn-primary btn-enrollment-button highlight outline`}
-                  >
+                  <div className={`btn btn-enrollment-button highlight`}>
                     Enrolled &#10003;
                   </div>
                 </Fragment>
