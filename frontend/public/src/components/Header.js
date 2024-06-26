95d6d0287667b52f8cb44b9cb098de7ead24e359
jenniw <jenniw@mit.edu>
2024-06-25 21:52:04 -0400

    configureScope becomes getCurrentScope (https://github.com/getsentry/sentry-javascript/blob/develop/MIGRATION.md#removal-of-sentryconfigurescope-method)

--- frontend/public/src/components/Header.js
+++ frontend/public/src/components/Header.js
@@ -15,14 +15,12 @@ type Props = {
 
 const Header = ({ currentUser, location }: Props) => {
   if (currentUser && currentUser.is_authenticated) {
-    Sentry.configureScope(scope => {
-      scope.setUser({
+    Sentry.getCurrentScope().setUser({
         id:       currentUser.id,
         email:    currentUser.email,
         username: currentUser.username,
         name:     currentUser.name
       })
-    })
     posthog.identify(currentUser.id, {
       environment: SETTINGS.environment,
       user_id:     currentUser.id
