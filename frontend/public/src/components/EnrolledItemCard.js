35f0e1425514f12bb5b83b5486d49edfea5b2fa1
CP <collinp@mit.edu>
2024-07-17 12:33:09 -0400

    page not live fix

--- frontend/public/src/components/EnrolledItemCard.js
+++ frontend/public/src/components/EnrolledItemCard.js
@@ -460,7 +460,8 @@ export class EnrolledItemCard extends React.Component<
 
     const onUnenrollClick = partial(this.onDeactivate.bind(this), [enrollment])
     const courseId = enrollment.run.course_number
-    const pageLocation = enrollment.run.course.page.live ?
+    console.log(enrollment.run.course.page)
+    const pageLocation = enrollment.run.course.page && enrollment.run.course.page.live ?
       enrollment.run.course.page :
       null
     const menuTitle = `Course options for ${enrollment.run.course.title}`
