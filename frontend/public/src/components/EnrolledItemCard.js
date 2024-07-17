78892e16fa5c5ebef3f657ed836c51046d0dfc59
cp-at-mit <collinp@mit.edu>
2024-07-17 15:58:36 -0400

    Enrollment in a course run without a course page crashes the dashboard (#2299)

--- frontend/public/src/components/EnrolledItemCard.js
+++ frontend/public/src/components/EnrolledItemCard.js
@@ -460,9 +460,10 @@ export class EnrolledItemCard extends React.Component<
 
     const onUnenrollClick = partial(this.onDeactivate.bind(this), [enrollment])
     const courseId = enrollment.run.course_number
-    const pageLocation = enrollment.run.course.page.live ?
-      enrollment.run.course.page :
-      null
+    const pageLocation =
+      enrollment.run.course.page && enrollment.run.course.page.live ?
+        enrollment.run.course.page :
+        null
     const menuTitle = `Course options for ${enrollment.run.course.title}`
 
     const courseRunStatusMessageText = courseRunStatusMessage(enrollment.run)
