a748ebfafff62af630b31d98d5e75ca83654a3b2
Anna <annagav@mit.edu>
2024-07-25 13:10:44 -0400

    noqa

--- courses/serializers/v2/programs_test.py
+++ courses/serializers/v2/programs_test.py
@@ -21,9 +21,9 @@ pytestmark = [pytest.mark.django_db]
 
 @pytest.mark.parametrize("remove_tree", [True, False])
 @pytest.mark.parametrize("certificate_type", ["MicroMasters Credential", "Certificate of Completion"])
-def test_serialize_program(
+def test_serialize_program(  # noqa: F811
     mock_context, remove_tree, certificate_type, program_with_empty_requirements
-):  # noqa: F811
+):
     """Test Program serialization"""
     if certificate_type == "MicroMasters Credential":
         program_with_empty_requirements.program_type = "MicroMasters®"
