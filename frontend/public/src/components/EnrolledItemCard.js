3504a6708fb01d1020db6f73cc3a1d3fc7ca104f
CP <collinp@mit.edu>
2024-08-26 14:03:27 -0400

    format

--- frontend/public/src/components/EnrolledItemCard.js
+++ frontend/public/src/components/EnrolledItemCard.js
@@ -438,13 +438,14 @@ export class EnrolledItemCard extends React.Component<
               <strong>Upgrade today</strong> and, upon passing, receive your
             certificate signed by MIT faculty to highlight the knowledge and
             skills you've gained from this MITx course.{" "}
-              {enrollment.run.upgrade_deadline ?
-              (<b>
-              Upgrade expires:{" "}
-                {formatPrettyDateTimeAmPmTz(
-                  parseDateString(enrollment.run.upgrade_deadline)
-                )}
-              </b>) : null}
+              {enrollment.run.upgrade_deadline ? (
+                <b>
+                Upgrade expires:{" "}
+                  {formatPrettyDateTimeAmPmTz(
+                    parseDateString(enrollment.run.upgrade_deadline)
+                  )}
+                </b>
+              ) : null}
             </div>
           </div>
         ) : null
