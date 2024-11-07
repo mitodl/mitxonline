2cb965bcd8c026d32d672f273a41a20a7249b31c
Anna <annagav@mit.edu>
2024-11-07 10:37:17 -0500

    refactoring

--- mail/verification_api.py
+++ mail/verification_api.py
@@ -1,15 +1,20 @@
 """API for email verifications"""
 
+import logging
 from urllib.parse import quote_plus
 
 from django.urls import reverse
 
-from authentication import serializers
+from rest_framework import serializers
+
 from mail import api
 from mail.constants import EMAIL_CHANGE_EMAIL, EMAIL_VERIFICATION
+from main.constants import USER_REGISTRATION_FAILED_MSG
 from openedx.api import validate_username_email_with_edx
 from openedx.exceptions import EdxApiRegistrationValidationException
 from users.serializers import OPENEDX_ACCOUNT_CREATION_VALIDATION_MSGS_MAP
+
+log = logging.getLogger()
 
 
 def send_verification_email(strategy, backend, code, partial_token):  # pylint: disable=unused-argument  # noqa: ARG001
@@ -22,8 +27,8 @@ def send_verification_email(strategy, backend, code, partial_token):  # pylint: 
         code (social_django.models.Code): the confirmation code used to confirm the email address
         partial_token (str): token used to resume a halted pipeline
     """
-    print("hello")
     email = strategy.request_data()["email"]
+    validate_email_with_edx_api(email)
 
     url = "{}?verification_code={}&partial_token={}".format(
         strategy.build_absolute_uri(reverse("register-confirm")),
@@ -39,21 +44,28 @@ def send_verification_email(strategy, backend, code, partial_token):  # pylint: 
         )
     )
 
+
 def validate_email_with_edx_api(email):
-        try:
-            openedx_validation_msg_dict = validate_username_email_with_edx({'email': email})
-        except (
-            EdxApiRegistrationValidationException,
-        ) as exc:
-            log.exception("Unable to create user account", exc)  # noqa: PLE1205, TRY401
-            raise serializers.ValidationError(USER_REGISTRATION_FAILED_MSG)  # noqa: B904
-        if openedx_validation_msg_dict["email"]:
-            # there is no email form field at this point, but we are still validating the email address
-            raise serializers.ValidationError({
-                    "email": OPENEDX_ACCOUNT_CREATION_VALIDATION_MSGS_MAP.get(
-                        openedx_validation_msg_dict["email"]
-                    )
-                })
+    """
+    Validate an email with edx api before sending a verification email
+
+    Args:
+        email (str): the email to validate
+    """
+    try:
+        openedx_validation_msg_dict = validate_username_email_with_edx({'email': email})
+    except (
+        EdxApiRegistrationValidationException,
+    ) as exc:
+        log.exception("Unable to create user account", exc)  # noqa: PLE1205, TRY401
+        raise serializers.ValidationError(USER_REGISTRATION_FAILED_MSG)  # noqa: B904
+    if openedx_validation_msg_dict["email"]:
+        # there is no email form field at this point, but we are still validating the email address
+        raise serializers.ValidationError({
+                "email": OPENEDX_ACCOUNT_CREATION_VALIDATION_MSGS_MAP.get(
+                    openedx_validation_msg_dict["email"]
+                )
+            })
 
 
 def send_verify_email_change_email(request, change_request):
