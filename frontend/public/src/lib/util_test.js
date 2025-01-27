363e0ee0f866c9c326bb4f79559e125b624c01ca
Anna <annagav@mit.edu>
2025-01-27 16:23:02 -0500

    locale

--- frontend/public/src/lib/util_test.js
+++ frontend/public/src/lib/util_test.js
@@ -180,8 +180,9 @@ describe("utility functions", () => {
   })
 
   it("formatPrettyDate should return a formatted moment date", () => {
+    moment.locale('en')
     const momentDate = moment("2019-01-01T00:00:00.000000Z")
-    assert.equal(formatPrettyDate(momentDate), "January 1, 2019")
+    assert.equal(formatPrettyDate(momentDate), "December 31, 2018")
   })
 
   it("firstItem should return the first item of an array", () => {
