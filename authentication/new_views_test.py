dd169596f2e59bb6901bf02739ae4436e044f19d
CP <collinp@mit.edu>
2025-05-06 09:59:47 -0400

    remove unused imports

--- authentication/new_views_test.py
+++ authentication/new_views_test.py
@@ -1,5 +1,3 @@
-from unittest.mock import MagicMock
-
 import pytest
 from django.test import RequestFactory
 from django.urls import reverse
@@ -7,7 +5,7 @@ from rest_framework import status
 
 from authentication.new_views import CustomLoginView
 from users.api import User
-from users.factories import UserFactory, UserProfileFactory
+from users.factories import UserFactory
 from users.models import MALE, UserProfile
 
 
