8660297308d268fcd834077f2e31868ff3c71a00
James Kachel <jkachel@mit.edu>
2025-05-07 16:12:15 -0500

    Add management command to copy email into username field (#2643)

---
+++ users/management/commands/migrate_usernames.py
@@ -0,0 +1,77 @@
+"""
+Change users' usernames so that they match their email addresses.
+"""
+
+import logging
+
+from django.contrib.auth import get_user_model
+from django.core.management import BaseCommand
+from django.db import transaction
+from django.db.utils import IntegrityError
+
+from users.constants import MIGRATE_USERNAME_BATCH_SIZE
+
+log = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    """
+    Change users' usernames so that they match their email addresses..
+    """
+
+    help = "Update users' usernames to match their email addresses."
+
+    def handle(self, *args, **kwargs):  # noqa: ARG002
+        """Set the username to the email address"""
+
+        User = get_user_model()
+        max_length = User._meta.get_field("username").max_length  # noqa: SLF001
+
+        users = User.objects.order_by("id").values("id", "email", "username").all()
+        total_users = users.count()
+        skipped_users = 0
+        already_migrated_users = 0
+
+        for start in range(0, total_users, MIGRATE_USERNAME_BATCH_SIZE):
+            batch = users[start : start + MIGRATE_USERNAME_BATCH_SIZE]
+            updates = []
+            new_usernames = []
+
+            for user in batch:
+                new_username = user["email"][:max_length]
+                if user["username"] == new_username:
+                    # If the user is already migrated, don't do it again.
+                    msg = f"User {user['id']} already has username {new_username}. Skipping update."
+                    log.info(msg)
+                    already_migrated_users += 1
+                else:
+                    updates.append(User(id=user["id"], username=new_username))
+                    new_usernames.append(new_username)
+
+            try:
+                with transaction.atomic():
+                    User.objects.bulk_update(updates, ["username"])
+            except IntegrityError:
+                # There was a username collision - step through this batch
+                # and update one at a time so we can figure out who it was
+                for user, new_username in zip(updates, new_usernames):
+                    if User.objects.filter(username=new_username).exists():
+                        msg = f"Username {new_username} already exists for user {user.id}. Skipping update."
+                        self.stdout.write(self.style.WARNING(msg))
+                        log.warning(msg)
+                        skipped_users += 1
+                    else:
+                        User.objects.filter(pk=user.id).update(username=new_username)
+
+        self.stdout.write(
+            self.style.SUCCESS(
+                f"Successfully updated {total_users - skipped_users - already_migrated_users}. {already_migrated_users} users were already migrated."
+            )
+        )
+
+        if skipped_users:
+            self.stdout.write(
+                self.style.WARNING(
+                    f"Skipped {skipped_users} users due to username collisions."
+                )
+            )
---
+++ users/management/tests/migrate_usernames_test.py
@@ -0,0 +1,49 @@
+"""Tests for the migrate_usernames command"""
+
+from io import StringIO
+
+import faker
+import pytest
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
+
+from users.constants import USERNAME_MAX_LEN
+from users.factories import UserFactory
+
+pytestmark = pytest.mark.django_db
+FAKE = faker.Faker()
+User = get_user_model()
+
+
+@pytest.mark.parametrize("break_one", [False, True])
+def test_migrate_usernames(break_one):
+    """Test that the migrate_usernames command works correctly with good data."""
+    out = StringIO()
+
+    # UserFactory uses fuzzytext, so just create a batch of them and run
+    # through it.
+
+    UserFactory.create_batch(10)
+
+    if break_one:
+        # Force a username collision - set a user's username to the same as
+        # another user's email address.
+        problem_email = FAKE.email()
+        user1 = UserFactory(email=problem_email)
+        UserFactory(username=problem_email[:USERNAME_MAX_LEN])
+
+    call_command(
+        "migrate_usernames",
+        stdout=out,
+    )
+
+    if break_one:
+        assert "Skipping update" in out.getvalue()
+        user1.refresh_from_db()
+        assert user1.username != user1.email[:USERNAME_MAX_LEN]
+    else:
+        assert "Successfully updated 10" in out.getvalue()
+
+    for user in User.objects.all():
+        if not break_one or user.id != user1.id:
+            assert user.username == user.email[:USERNAME_MAX_LEN]
