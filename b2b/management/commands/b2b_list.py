2c7cd2a07f5f9690afb853516df73e865676fbe2
James Kachel <jkachel@mit.edu>
2025-05-12 10:27:58 -0500

    Add support for listing out the codes for a contract

--- b2b/management/commands/b2b_list.py
+++ b2b/management/commands/b2b_list.py
@@ -1,8 +1,13 @@
 """List B2B data."""
 
+import csv
 import logging
+from pathlib import Path
+from urllib.parse import urlencode, urljoin
 
+from django.conf import settings
 from django.core.management import BaseCommand, CommandError
+from django.urls import reverse
 from rich.console import Console
 from rich.table import Table
 
@@ -16,6 +21,33 @@ class Command(BaseCommand):
     """List B2B data."""
 
     help = "List B2B data."
+
+    def _fmt_redemptions(self, code):
+        """Format the redemptions for a code."""
+
+        if code.order_redemptions.count() == 0:
+            return "No"
+        elif code.order_redemptions.count() == 1:
+            return code.order_redemptions.first().redemeed_by.email
+
+        return code.order_redemptions.count()
+
+    def _gen_product_url(self, product):
+        """Generate the product URL."""
+
+        qs = urlencode(
+            {
+                "product_id": product.id,
+            }
+        )
+
+        return urljoin(
+            settings.SITE_BASE_URL,
+            urljoin(
+                reverse("checkout-product"),
+                f"?{qs}",
+            ),
+        )
 
     def add_arguments(self, parser):
         """Add command line arguments."""
@@ -48,6 +80,17 @@ class Command(BaseCommand):
             type=int,
             help="Filter by organization ID.",
             dest="organization_id",
+        )
+        contract_parser.add_argument(
+            "--codes",
+            type=int,
+            help="List enrollment codes for the specified contract.",
+        )
+        contract_parser.add_argument(
+            "--codes-out",
+            type=str,
+            help="Output file for enrollment codes.",
+            default="",
         )
 
         courseware_parser = subparsers.add_parser(
@@ -116,6 +159,37 @@ class Command(BaseCommand):
     def handle_list_contracts(self, *args, **kwargs):  # noqa: ARG002
         """Handle the list subcommand."""
         org_id = kwargs.pop("organization_id")
+        codes_contract_id = kwargs.pop("codes")
+
+        if codes_contract_id:
+            output_file = kwargs.pop("codes_out")
+            if not output_file:
+                output_file = f"codes-{codes_contract_id}.csv"
+            contract = ContractPage.objects.get(id=codes_contract_id)
+
+            with Path(output_file).open("w") as f:
+                csv_writer = csv.writer(f)
+                csv_writer.writerow(
+                    [
+                        "Code",
+                        "Product",
+                        "Redeemed",
+                        "Product URL",
+                    ]
+                )
+
+                for code in contract.get_discounts():
+                    csv_writer.writerow(
+                        [
+                            code.discount_code,
+                            code.products.first().product,
+                            self._fmt_redemptions(code),
+                            self._gen_product_url(code.products.first().product),
+                        ]
+                    )
+
+            self.stdout.write(self.style.SUCCESS(f"Codes written to {output_file}"))
+            return
 
         if org_id:
             contracts = ContractPage.objects.filter(organization__id=org_id).all()
