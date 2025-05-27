79a8aa56eb3967b44aec15e5378c738efbcbf390
CP <collinp@mit.edu>
2025-05-27 07:02:10 -0400

    hold

--- authentication/api.py
+++ authentication/api.py
@@ -33,34 +33,3 @@ def create_user_session(user):
 
     return session
 
-
-def create_user_with_generated_username(serializer, initial_username):
-    """
-    Creates a User with a given username, and if there is a User that already has that username,
-    finds an available username and reattempts the User creation.
-
-    Args:
-        serializer (UserSerializer instance): A user serializer instance that has been instantiated
-            with user data and has passed initial validation
-        initial_username (str): The initial username to attempt to save the User with
-    Returns:
-        User or None: The created User (or None if the User could not be created in the
-            number of retries allowed)
-    """
-    created_user = None
-    username = initial_username
-    attempts = 0
-
-    if len(username) < 2:  # noqa: PLR2004
-        username = username + "11"
-
-    while created_user is None and attempts < USERNAME_COLLISION_ATTEMPTS:
-        try:
-            created_user = serializer.save(username=username)
-        except IntegrityError as exc:  # noqa: PERF203
-            if not is_duplicate_username_error(exc):
-                raise
-            username = find_available_username(initial_username)
-        finally:
-            attempts += 1
-    return created_user
--- authentication/api_test.py
+++ authentication/api_test.py
@@ -1,11 +1,8 @@
 """API tests"""
 
 import pytest
-from django.db import IntegrityError
 
 from authentication import api
-from users.factories import UserFactory
-from users.serializers import UserSerializer
 
 pytestmark = pytest.mark.django_db
 
@@ -16,74 +13,3 @@ def test_create_user_session(user):
     assert session is not None
     assert session.session_key is not None
 
-
-def test_create_user_reattempt(mocker):
-    """
-    Test that create_user_with_generated_username reattempts User creation multiple times when
-    username collisions are experienced repeatedly
-    """
-    username = "testuser"
-    fake_user = UserFactory.create()
-    patched_find_username = mocker.patch(
-        "authentication.api.find_available_username",
-        side_effect=["testuser1", "testuser2", "testuser3"],
-    )
-    patched_save = mocker.patch.object(
-        UserSerializer,
-        "save",
-        side_effect=[
-            IntegrityError("(username)=(testuser) already exists"),
-            IntegrityError("(username)=(testuser1) already exists"),
-            IntegrityError("(username)=(testuser2) already exists"),
-            fake_user,
-        ],
-    )
-
-    created_user = api.create_user_with_generated_username(
-        UserSerializer(data={}), username
-    )
-    assert patched_save.call_count == 4
-    patched_save.assert_any_call(username="testuser")
-    patched_save.assert_any_call(username="testuser1")
-    patched_save.assert_any_call(username="testuser2")
-    patched_save.assert_any_call(username="testuser3")
-    # `find_available_username` should be called as many times as serializer.save() failed
-    # with a duplicate username error
-    assert patched_find_username.call_count == 3
-    patched_find_username.assert_called_with(username)
-    assert created_user == fake_user
-
-
-def test_create_user_too_many_attempts(mocker):
-    """
-    Test that create_user_with_generated_username exits if there are too many attempts
-    """
-    attempt_limit = 2
-    mocker.patch("authentication.api.USERNAME_COLLISION_ATTEMPTS", attempt_limit)
-    patched_save = mocker.patch.object(
-        UserSerializer,
-        "save",
-        side_effect=(IntegrityError("(username)=(testuser) already exists")),
-    )
-    patched_find_username = mocker.patch(
-        "authentication.api.find_available_username", return_value=None
-    )
-    created_user = api.create_user_with_generated_username(
-        UserSerializer(data={}), "testuser"
-    )
-    assert created_user is None
-    assert patched_save.call_count == attempt_limit
-    assert patched_find_username.call_count == attempt_limit
-
-
-def test_create_user_exception(mocker):
-    """
-    Test that create_user_with_generated_username does not reattempt if an exception was raised that
-    does not indicate a username collision
-    """
-    patched_save = mocker.patch.object(
-        UserSerializer, "save", side_effect=ValueError("idk")
-    )
-    with pytest.raises(ValueError):  # noqa: PT011
-        api.create_user_with_generated_username(UserSerializer(data={}), "testuser")
-    patched_save.assert_called_once()
