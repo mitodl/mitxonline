79146570e4d3a47689119c53a4310ce7e8577de9
Carey Gumaer <gumaerc@mit.edu>
2025-07-07 12:17:49 -0400

    remove extra authentication middleware line

--- main/settings.py
+++ main/settings.py
@@ -324,7 +324,6 @@ MIDDLEWARE = (
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "mitol.apigateway.middleware.ApisixUserMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
     "django.contrib.sites.middleware.CurrentSiteMiddleware",
