f61ac59be1c17a756b9aa5f258bb1a8290ebb06f
James Kachel <jkachel@mit.edu>
2025-07-18 15:30:43 -0500

    Finishing up tests; fixed some issues with the management command

--- courses/management/commands/fix_eligible_course_flags.py
+++ courses/management/commands/fix_eligible_course_flags.py
@@ -17,8 +17,13 @@ overwrite the flags so running it without filters should be done with care.
 from django.core.management import BaseCommand
 from django.db.models import Count, Q
 
-from courses.models import Course, ProgramRequirement, ProgramRequirementNodeType
-from courses.utils import get_enrollable_courses
+from courses.models import (
+    Course,
+    CourseRun,
+    ProgramRequirement,
+    ProgramRequirementNodeType,
+)
+from courses.utils import get_enrollable_course_run_filter
 
 
 class Command(BaseCommand):
@@ -31,7 +36,7 @@ class Command(BaseCommand):
 
         parser.add_argument(
             "--dry-run",
-            type=bool,
+            action="store_true",
             help="Determine what needs to change, but don't make any changes.",
         )
         parser.add_argument(
@@ -52,7 +57,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):  # noqa: ARG002
         """Get the courses and figure out what changes to make"""
 
-        dry_run = options["dry_run"]
+        dry_run = options.get("dry_run", False)
+        filter_courses = options.get("course", False)
+        filter_programs = options.get("program", False)
 
         if dry_run:
             self.stdout.write(
@@ -62,31 +69,44 @@ class Command(BaseCommand):
             )
 
         courses_base_qs = (
-            get_enrollable_courses(Course.objects.all())
+            Course.objects.all()
             .annotate(
-                Count("courseruns", filter=Q(courseruns__b2b_contract__isnull=True)),
-                "regular_courserun_count",
+                enrollable_runs=Count(
+                    "courseruns",
+                    filter=Q(
+                        courseruns__in=CourseRun.objects.filter(
+                            get_enrollable_course_run_filter()
+                        )
+                    ),
+                ),
             )
             .annotate(
-                Count("courseruns", filter=Q(courseruns__b2b_contract__isnull=False)),
-                "b2b_courserun_count",
+                regular_courserun_count=Count(
+                    "courseruns", filter=Q(courseruns__b2b_contract__isnull=True)
+                ),
+            )
+            .annotate(
+                b2b_courserun_count=Count(
+                    "courseruns", filter=Q(courseruns__b2b_contract__isnull=False)
+                ),
             )
         )
 
-        if "course" not in options and "program" not in options:
+        if not filter_courses and not filter_programs:
             self.stdout.write(
                 self.style.WARNING(
                     "Warning - no filter applied. Changes will apply to all course records."
                 )
             )
 
-        if "course" in options:
-            courses_base_qs = courses_base_qs.filter(readable_id__in=options["course"])
-        if "program" in options:
+        if filter_courses and len(filter_courses) > 0:
+            courses_base_qs = courses_base_qs.filter(readable_id__in=filter_courses)
+
+        if filter_programs and len(filter_programs) > 0:
             # get the programs, then get the courses in their requirements, something like that
             program_req_readable_ids = (
                 ProgramRequirement.objects.filter(
-                    program__readable_id__in=options["program"],
+                    program__readable_id__in=filter_programs,
                     node_type=ProgramRequirementNodeType.COURSE,
                 )
                 .select_related("course")
@@ -95,11 +115,11 @@ class Command(BaseCommand):
             )
 
             courses_base_qs = courses_base_qs.filter(
-                readable_id_in=program_req_readable_ids
+                readable_id__in=program_req_readable_ids
             )
 
         ingest_qs = courses_base_qs
-        ingest_qs = ingest_qs.filter(b2b_courserun_count_gt=0).all()
+        ingest_qs = ingest_qs.filter(Q(b2b_courserun_count__gt=0) | Q(page__live=True))
 
         updated_count = (
             ingest_qs.count()
@@ -113,7 +133,7 @@ class Command(BaseCommand):
 
         include_qs = courses_base_qs
         include_qs = include_qs.filter(
-            Q(regular_courserun_count=0) | Q(page__live__istrue=True)
+            regular_courserun_count__gt=0, page__live=True
         ).all()
 
         updated_count = (
@@ -125,3 +145,21 @@ class Command(BaseCommand):
         self.stdout.write(
             self.style.SUCCESS(f"Set Learn catalog flag on {updated_count} courses")
         )
+
+        if dry_run:
+            self.stdout.write("Would have adjusted these courses for ingestion:")
+            [
+                self.stdout.write(
+                    f"{course.readable_id}: {course.ingest_content_files_for_ai} -> True"
+                )
+                for course in ingest_qs
+            ]
+            self.stdout.write(
+                "Would have adjusted these courses for inclusion in Learn catalog:"
+            )
+            [
+                self.stdout.write(
+                    f"{course.readable_id}: {course.include_in_learn_catalog} -> True"
+                )
+                for course in include_qs
+            ]
