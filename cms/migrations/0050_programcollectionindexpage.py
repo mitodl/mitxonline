ae4545858369bc67e5d9b429b708a8b6619cbb12
CP <collinp@mit.edu>
2025-07-23 11:02:55 -0400

    fix

--- cms/migrations/0050_programcollectionindexpage.py
+++ cms/migrations/0050_programcollectionindexpage.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
     dependencies = [
         ("wagtailcore", "0094_alter_page_locale"),
-        ("cms", "0048_alter_coursepage_max_price_and_more"),
+        ("cms", "0049_add_course_flag_fields"),
     ]
 
     operations = [
