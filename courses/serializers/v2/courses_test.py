00ac3cbe1e828ffc6af81189be21eb4b9aae8302
CP <collinp@mit.edu>
2025-07-31 15:47:55 -0400

    Revert "REmove duplicate contract ID"
    This reverts commit f945c7c76bb2d00b586c9908090c0ebe2e02988a.

--- courses/serializers/v2/courses_test.py
+++ courses/serializers/v2/courses_test.py
@@ -146,9 +146,9 @@ class TestCourseRunEnrollmentSerializerV2:
         serialized_data = CourseRunEnrollmentSerializer(enrollment).data
 
         assert "b2b_organization_id" in serialized_data
-        assert "b2b_contract" in serialized_data
+        assert "b2b_contract_id" in serialized_data
         assert serialized_data["b2b_organization_id"] is None
-        assert serialized_data["b2b_contract"] is None
+        assert serialized_data["b2b_contract_id"] is None
 
     def test_serializer_with_b2b_contract(self):
         """Test serialization with B2B contract."""
@@ -161,7 +161,7 @@ class TestCourseRunEnrollmentSerializerV2:
 
         serialized_data = CourseRunEnrollmentSerializer(enrollment).data
         assert serialized_data["b2b_organization_id"] == org.id
-        assert serialized_data["b2b_contract"] == contract.id
+        assert serialized_data["b2b_contract_id"] == contract.id
 
     def test_serializer_fields(self):
         """Test that all expected fields are present."""
@@ -177,7 +177,7 @@ class TestCourseRunEnrollmentSerializerV2:
             "certificate",
             "grades",
             "b2b_organization_id",
-            "b2b_contract",
+            "b2b_contract_id",
         }
 
         assert set(serialized_data.keys()) == expected_fields
