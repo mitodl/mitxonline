cb48ef1be30a79be54662123cdf491830ad4c56b
James Kachel <jkachel@mit.edu>
2025-08-05 13:49:03 -0500

    clean up some debug stuff, remove a debug middleware

--- main/settings.py
+++ main/settings.py
@@ -333,7 +333,6 @@ MIDDLEWARE = (
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "main.middleware.apisix_debug_middleware",
     "mitol.apigateway.middleware.ApisixUserMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "main.middleware.HostBasedCSRFMiddleware",
