cde3c98cb2f7ebe97ae7dcd665938da22f7e00b4
James Kachel <james@jkachel.com>
2025-08-27 08:05:43 -0700

    save generated username in desired field too since locking uses this

--- openedx/api.py
+++ openedx/api.py
@@ -368,9 +368,11 @@ def reconcile_edx_username(user, *, desired_username=None):
                     )
 
                 if not OpenEdxUser.objects.filter(
-                    edx_username=username_to_try
+                    edx_username=username_to_try,
+                    desired_edx_username=username_to_try,
                 ).exists():
                     edx_user.edx_username = username_to_try
+                    edx_user.desired_edx_username = username_to_try
                     edx_user.save()
                     break
 
