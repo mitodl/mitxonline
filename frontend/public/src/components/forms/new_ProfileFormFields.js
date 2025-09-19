82d6dff693b2910cfeec4e2aec5db3d6cb14f2d1
Anna <annagav@mit.edu>
2025-09-19 13:52:28 -0400

    no-useless-escape

--- frontend/public/src/components/forms/new_ProfileFormFields.js
+++ frontend/public/src/components/forms/new_ProfileFormFields.js
@@ -34,7 +34,7 @@ export const legalAddressValidation = yup.object().shape({
     .min(3, usernameFieldErrorMessage)
     .max(30, usernameFieldErrorMessage)
     .matches(
-      /^[^!@#$%^&*()+=[]{};':"\\|,<>\/?~]*$/,
+      /^[^!@#$%^&*()+=\[\]{};':"\\|,<>\/?~]*$/, // eslint-disable-line no-useless-escape
       "Username cannot contain any of these !@#$%^&*()+=[]{};':\"\\|,<>/?~ symbols"
     ),
   legal_address: yup.object().shape({
