7d3348a73c10a8d11696ae6217cd1059f5b61ba6
CP <collinp@mit.edu>
2025-10-07 05:04:03 -0400

    fix

--- frontend/public/src/containers/pages/login/LoginSso.js
+++ frontend/public/src/containers/pages/login/LoginSso.js
@@ -1,9 +1,20 @@
 // @flow
-import React from "react"
-import { Redirect } from "react-router-dom"
+import React, { useEffect } from "react"
+import qs from "query-string"
 
 const LoginSso = () => {
-  return <Redirect to="/login" />
+  useEffect(() => {
+    const nextUrl = new URLSearchParams(window.location.search).get('next') || window.location.pathname
+    const params = qs.stringify({ next: nextUrl })
+    
+    window.location.href = `/login/?${params}`
+  }, [])
+
+  return (
+    <div className="loading-container">
+      <div>Redirecting to login...</div>
+    </div>
+  )
 }
 
 export default LoginSso
