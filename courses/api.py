d096fc7e50ce5746ad52c6b665c87442720212c8
James Kachel <jkachel@mit.edu>
2025-10-30 15:47:07 -0500

    Swap over the management command to use the API call instead

--- courses/api.py
+++ courses/api.py
@@ -1212,7 +1212,7 @@ def import_courserun_from_edx(  # noqa: C901, PLR0913
                 else:
                     dept = department
 
-                root_course.departments.add(dept)
+                root_course.departments.add(dept.id)
 
     new_run = CourseRun.objects.create(
         course=root_course,
