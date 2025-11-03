4c39c6dcae9380f3637bd5716e58f4aa99fdb7a9
CP <collinp@mit.edu>
2025-11-03 12:19:15 -0500

    remove programs url to cause error

--- courses/urls/v2/urls.py
+++ courses/urls/v2/urls.py
@@ -7,7 +7,6 @@ from courses.views import v2
 
 app_name = "courses"
 router = routers.SimpleRouter()
-router.register(r"programs", v2.ProgramViewSet, basename="programs_api")
 router.register(
     r"program-collections",
     v2.ProgramCollectionViewSet,
