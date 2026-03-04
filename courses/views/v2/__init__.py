8d416e4ecad5ce135056756cb9be34a4338c000e
Anna <annagav@mit.edu>
2026-03-04 16:38:19 -0500

    wrong import

--- courses/views/v2/__init__.py
+++ courses/views/v2/__init__.py
@@ -6,7 +6,8 @@ import logging
 
 import django_filters
 from django.conf import settings
-from django.db.models import Count, GenericPrefetch, Prefetch, Q
+from django.contrib.contenttypes.prefetch import GenericPrefetch
+from django.db.models import Count, Prefetch, Q
 from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
