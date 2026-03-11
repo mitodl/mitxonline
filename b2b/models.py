9d1ecf0c88ba817c70072b29ba320c2070122c3c
James Kachel <jkachel@mit.edu>
2026-03-11 16:27:35 -0500

    Live-fire testing - fixed a few bugs

--- b2b/models.py
+++ b2b/models.py
@@ -356,6 +356,8 @@ class ContractPage(Page, ClusterableModel):
                 FieldPanel("membership_type"),
                 FieldPanel("max_learners"),
                 FieldPanel("enrollment_fixed_price"),
+                FieldPanel("google_sheet_target"),
+                FieldPanel("google_sheet_target_tab"),
             ],
             heading="Learner Provisioning",
             icon="user",
@@ -478,19 +480,26 @@ class ContractPage(Page, ClusterableModel):
             object_id__in=self.get_course_runs().values_list("id", flat=True),
         ).all()
 
-    def get_discounts(self):
-        """Get the discounts associated with the contract."""
+    @property
+    def discounts_qs(self):
+        """Get the discount queryset associated with the contract."""
 
         from ecommerce.models import Discount  # noqa: PLC0415
 
-        return Discount.objects.filter(products__product__in=self.get_products()).all()
+        return Discount.objects.filter(products__product__in=self.get_products())
+
+    def get_discounts(self):
+        """Get the discounts associated with the contract."""
+
+        return self.discounts_qs.all()
 
     def get_unused_discounts(self):
         """Get discounts that haven't been used yet."""
 
         return (
-            self.get_discounts()
-            .annotate(order_redemptions_count=models.Count("order_redemptions"))
+            self.discounts_qs.annotate(
+                order_redemptions_count=models.Count("order_redemptions")
+            )
             .annotate(contract_redemptions_count=models.Count("contract_redemptions"))
             .exclude(
                 models.Q(order_redemptions_count__gt=0)
--- b2b/sheets.py
+++ b2b/sheets.py
@@ -7,7 +7,10 @@ from mitol.google_sheets.api import get_authorized_pygsheets_client
 from mitol.google_sheets.constants import GOOGLE_SHEET_FIRST_ROW
 from mitol.google_sheets.sheet_handler_api import SheetHandler
 
+from b2b.constants import CONTRACT_MEMBERSHIP_AUTOS
 from b2b.models import ContractPage
+from ecommerce.constants import REDEMPTION_TYPE_UNLIMITED
+from ecommerce.models import Discount
 
 log = logging.getLogger(__name__)
 
@@ -26,30 +29,38 @@ class ContractEnrollmentCodesSheetHandler(SheetHandler):
 
     def __init__(
         self,
-        spreadsheet_url: str,
-        worksheet_name: str | int,
-        *,
-        start_row: int | None = 0,
+        contract_page: ContractPage,
     ):
         """
         Initialize the class.
 
-        Unlike the deferrals and refunds code, this expects a spreadsheet URL. It
-        expects to have to work on a number of different sheets that are created
-        by a number of potentially non-technical people so it seems easier to
-        accept a URL.
+        This uses the same base as the deferrals and refunds handling code,
+        but it splits from it a lot. This expects a GSheets URL rather than an
+        ID, and it expects those to come from the ContractPage, because we allow
+        for each contract to have its own page. Same with the sheet tab.
+
+        This will also raise a ValueError if you pass in a ContractPage that does
+        not have enrollment codes.
 
         Args:
-            spreadsheet_url (str): the spreadsheet to use
-            worksheet_name (str|int): the worksheet (tab) name or index to use
-            start_row (int): starting row (optional, starts at 0)
-            sheet_metadata (Type(SheetConfig)):
-            request_model_cls (Type(GoogleSheetsRequestModel)):
+            contract_page (ContractPage): the contract to work with
         """
+
+        if not contract_page.google_sheet_target:
+            msg = f"Contract {contract_page} has no linked Google Sheet, can't continue"
+            raise ValueError(msg)
+
+        if contract_page.membership_type in CONTRACT_MEMBERSHIP_AUTOS:
+            msg = f"Membership for contract {contract_page} is managed; no enrollment codes, so can't continue"
+            raise ValueError(msg)
+
+        self.contract_page = contract_page
         self.pygsheets_client = get_authorized_pygsheets_client()
-        self.spreadsheet = self.pygsheets_client.open_by_url(spreadsheet_url)
-        self.worksheet_name = worksheet_name
-        self.start_row = start_row
+        self.spreadsheet = self.pygsheets_client.open_by_url(
+            contract_page.google_sheet_target
+        )
+        self.worksheet_name = contract_page.google_sheet_target_tab
+        self.start_row = 0  # unimplemented at this point
 
     @cached_property
     def worksheet(self):
@@ -61,13 +72,13 @@ class ContractEnrollmentCodesSheetHandler(SheetHandler):
         )
 
     @property
-    def row_zero(self):
+    def row_zero(self) -> int:
         """Gets the first row we're supposed to use."""
 
         return GOOGLE_SHEET_FIRST_ROW + self.start_row
 
     @property
-    def row_one(self):
+    def row_one(self) -> int:
         """Gets the first row for data, based on row_zero."""
 
         return self.row_zero + 1
@@ -76,6 +87,34 @@ class ContractEnrollmentCodesSheetHandler(SheetHandler):
         """Write/overwrite the header row."""
 
         self.worksheet.update_row(self.row_zero, self.default_columns)
+
+    def _get_discount_cells(self, discount: Discount) -> list:
+        """Format the discount for the sheet"""
+
+        redemption_date = ""
+        redeemed_by = ""
+        redeemed_on = ""
+
+        if (
+            discount.redemption_type != REDEMPTION_TYPE_UNLIMITED
+            and discount.contract_redemptions.count() > 0
+        ):
+            redemption_date = str(discount.contract_redemptions.get().created_on)
+
+        if discount.contract_redemptions.exists():
+            redeemed_by = discount.contract_redemptions.last().user.email
+            redeemed_on = str(discount.contract_redemptions.last().created_on)
+
+        return [
+            discount.discount_code,
+            discount.redemption_type,
+            discount.contract_redemptions.count(),
+            str(discount.expiration_date)
+            if discount.expiration_date
+            else redemption_date,
+            redeemed_by,
+            redeemed_on,
+        ]
 
     def ensure_header(self):
         """Ensure there's a header row in the worksheet."""
@@ -87,11 +126,14 @@ class ContractEnrollmentCodesSheetHandler(SheetHandler):
             return
 
         for idx, rowvalue in enumerate(header_row_values):
+            if idx >= len(self.default_columns):
+                break
+
             if rowvalue != self.default_columns[idx]:
                 self._write_header()
                 return
 
-    def write_codes(self, contract: ContractPage):
+    def write_codes(self) -> int:
         """
         Write out all the enrollment codes for the contract.
 
@@ -100,12 +142,62 @@ class ContractEnrollmentCodesSheetHandler(SheetHandler):
         the codes. Use the command for that if you need it.
         """
 
-        return
+        self.ensure_header()
 
-    def check_code(self, enrollment_code: str):
+        row_idx = self.row_one
+        codes = []
+
+        for code in self.contract_page.discounts_qs.prefetch_related(
+            "contract_redemptions", "contract_redemptions__user"
+        ).all()[: self.contract_page.max_learners]:
+            col = self._get_discount_cells(code)
+
+            code.b2b_sheet_location = row_idx
+            codes.append(code)
+
+            self.worksheet.update_row(row_idx, col)
+
+            row_idx += 1
+
+        Discount.objects.bulk_update(codes, {"b2b_sheet_location"})
+
+        return row_idx - self.row_one
+
+    def check_code(self, enrollment_code: str) -> int:
         """Check for the given enrollment code in the sheet."""
 
-        return enrollment_code
+        discount = self.contract_page.discounts_qs.filter(
+            discount_code=enrollment_code
+        ).get()
+
+        if discount.b2b_sheet_location:
+            cached_row = self.worksheet.get_row(
+                discount.b2b_sheet_location, tailing_empty=False, returnas="matrix"
+            )
+
+            if cached_row[0] == discount.discount_code:
+                return discount.b2b_sheet_location
+
+        found_cell = self.worksheet.find(
+            discount.discount_code,
+            matchEntireCell=True,
+            rows=(self.row_one, None),
+            cols=(1, 1),
+        )
+
+        if len(found_cell) == 0:
+            return False
+
+        if len(found_cell) > 1:
+            found_locs = ",".join([f"({cell.row},{cell.col})" for cell in found_cell])
+            msg = f"Code {discount.discount_code} seems to be in the sheet more than once: {found_locs}"
+
+            raise ValueError(msg)
+
+        discount.b2b_sheet_location = found_cell[0].row
+        discount.b2b_sheet_location.save()
+
+        return found_cell[0].row
 
     def update_code(self, enrollment_code: str):
         """
@@ -116,4 +208,12 @@ class ContractEnrollmentCodesSheetHandler(SheetHandler):
         the code.
         """
 
-        return enrollment_code
+        discount = self.contract_page.discounts_qs.filter(
+            discount_code=enrollment_code
+        ).get()
+
+        row = self.check_code(enrollment_code)
+
+        update_col = self._get_discount_cells(discount)
+
+        self.worksheet.update_row(row, update_col)
