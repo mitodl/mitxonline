b5f3233e40763de071b2e806f12c93435316de98
dansubak <dansubak@mit.edu>
2026-03-12 15:59:04 -0400

    Induce failure in one of the early scripts

--- scripts/test/detect_missing_migrations.sh
+++ scripts/test/detect_missing_migrations.sh
@@ -8,11 +8,12 @@ fail() {
 	exit 1
 }
 
-MIGRATIONS_OUTPUT="$(uv run ./manage.py makemigrations --no-input --dry-run 2>&1)"
-if [[ $? -ne 0 ]]; then
-	# makemigrations has returned a non-zero for some reason, possibly
-	# because it needs input but --no-input is set
-	fail
-elif [[ $(echo "$MIGRATIONS_OUTPUT" | grep "No changes detected" | wc -l) -eq 0 ]]; then
-	fail
-fi
+fail
+#MIGRATIONS_OUTPUT="$(uv run ./manage.py makemigrations --no-input --dry-run 2>&1)"
+#if [[ $? -ne 0 ]]; then
+#	# makemigrations has returned a non-zero for some reason, possibly
+#	# because it needs input but --no-input is set
+#	fail
+#elif [[ $(echo "$MIGRATIONS_OUTPUT" | grep "No changes detected" | wc -l) -eq 0 ]]; then
+#	fail
+#fi
