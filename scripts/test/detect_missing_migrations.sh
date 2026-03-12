618aecd6fed08010a870d184126b9c629c0098ef
dansubak <dansubak@mit.edu>
2026-03-12 16:04:12 -0400

    Remove induced failure

--- scripts/test/detect_missing_migrations.sh
+++ scripts/test/detect_missing_migrations.sh
@@ -8,12 +8,11 @@ fail() {
 	exit 1
 }
 
-fail
-#MIGRATIONS_OUTPUT="$(uv run ./manage.py makemigrations --no-input --dry-run 2>&1)"
-#if [[ $? -ne 0 ]]; then
-#	# makemigrations has returned a non-zero for some reason, possibly
-#	# because it needs input but --no-input is set
-#	fail
-#elif [[ $(echo "$MIGRATIONS_OUTPUT" | grep "No changes detected" | wc -l) -eq 0 ]]; then
-#	fail
-#fi
+MIGRATIONS_OUTPUT="$(uv run ./manage.py makemigrations --no-input --dry-run 2>&1)"
+if [[ $? -ne 0 ]]; then
+	# makemigrations has returned a non-zero for some reason, possibly
+	# because it needs input but --no-input is set
+	fail
+elif [[ $(echo "$MIGRATIONS_OUTPUT" | grep "No changes detected" | wc -l) -eq 0 ]]; then
+	fail
+fi
