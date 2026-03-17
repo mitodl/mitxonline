40d9810af0b26b5785ea72cf1599fa926aad32cf
James Kachel <jkachel@mit.edu>
2026-03-17 15:02:58 -0500

    Stop passing the edx_client to the on_commit steps

--- openedx/api.py
+++ openedx/api.py
@@ -1627,14 +1627,8 @@ def process_course_run_clone(target_course: CourseRun, base_course_key: str):
     # We should have the target course in edX now. We need to update it with the
     # data from our course run.
     # Run these after the transaction that this will most likely be
-    transaction.on_commit(
-        partial(fix_cloned_run_data, target_course=target_course, edx_client=edx_client)
-    )
-    transaction.on_commit(
-        partial(
-            push_edx_modes_from_run, course_run=target_course, edx_client=edx_client
-        )
-    )
+    transaction.on_commit(partial(fix_cloned_run_data, target_course=target_course))
+    transaction.on_commit(partial(push_edx_modes_from_run, course_run=target_course))
 
 
 def get_edx_course_modes(
