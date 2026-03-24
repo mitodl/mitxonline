e0c3d89c150a60015feb06e33d2844bcea11bf75
Nathan Levesque <rhysyngsun@users.noreply.github.com>
2026-03-24 09:43:48 -0500

    Don't allow purchased programs to be unenrolled (#3413)

--- courses/views/v2/__init__.py
+++ courses/views/v2/__init__.py
@@ -897,6 +897,13 @@ class UserProgramEnrollmentsViewSet(viewsets.ViewSet):
             user=request.user, program=program
         ).first()
         if enrollment:
+            if not enrollment.can_unenroll:
+                return Response(
+                    {
+                        "message": "Cannot unenroll from a purchased program, contact support."
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
             enrollment.deactivate_and_save(ENROLL_CHANGE_STATUS_UNENROLLED)
 
         return self.list(request)
