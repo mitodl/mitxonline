420d4fb95366ed634d0d44c4f34c12129d59a8fe
zamanafzal <zamanafzal@gmail.com>
2026-03-31 13:58:22 +0500

    fix: fix tests

--- courses/utils_test.py
+++ courses/utils_test.py
@@ -310,6 +310,7 @@ def test_is_contract_order_false_for_non_course_run_product():
     assert is_contract_order(order) is False
 
 
+@pytest.mark.django_db
 def test_is_uai_course_run_with_legacy_courseware_id():
     """UAI detection should work for legacy course-v1 prefixed keys."""
     course_run = CourseRunFactory.create(
@@ -318,12 +319,14 @@ def test_is_uai_course_run_with_legacy_courseware_id():
     assert is_uai_course_run(course_run) is True
 
 
+@pytest.mark.django_db
 def test_is_uai_course_run_with_org_prefix_only():
     """UAI detection should work for org-prefix-only keys."""
     course_run = CourseRunFactory.create(courseware_id="UAI_MIT+1.001x+2025_C12")
     assert is_uai_course_run(course_run) is True
 
 
+@pytest.mark.django_db
 def test_is_uai_course_run_with_non_uai_courseware_id():
     """UAI detection should return False for non-UAI keys."""
     course_run = CourseRunFactory.create(courseware_id="course-v1:MITx+6.00.1x+1T2026")
