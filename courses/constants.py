5fff7c8f3c992f16a354827ad418307a66493ef5
James Kachel <jkachel@mit.edu>
2026-05-21 15:48:43 -0500

    WIP: first bit of code; added enums for languages, industries, lengths; added fields and migration; added to API output and included for filtering in v2/courses
    more to do and no tests, but good stopping point

--- courses/constants.py
+++ courses/constants.py
@@ -56,3 +56,182 @@ COURSE_KEY_PATTERN = r"^course-v1:[^+]+\+[^+]+\+[^+]+$"
 #
 # The generated URL uses the pattern: {OPENEDX_BASE_REDIRECT_URL}/learn/course/{readable_id}/home
 COURSEWARE_URL_PATTERN_TEMPLATE = "/learn/course/{courseware_id}/home"
+
+# Course Variant Settings
+
+COURSE_VARIANT_INDUSTRY = [
+    ("", "Original"),
+    (
+        "E",
+        "Energy",
+    ),
+    (
+        "F",
+        "Finance",
+    ),
+    ("HC", "Healthcare"),
+]
+
+COURSE_VARIANT_LENGTH = [
+    (
+        "",
+        "Normal",
+    ),
+    (
+        "S",
+        "Short",
+    ),
+    (
+        "L",
+        "Long",
+    ),
+]
+
+# These are mostly ISO 639-1 codes, except sometimes they have a locale code
+# appended. The list is from openedx-translations.
+COURSE_VARIANT_LANGUAGE = [
+    ("af_ZA", "af_ZA"),
+    (
+        "ar",
+        "ar",
+    ),
+    (
+        "az",
+        "az",
+    ),
+    (
+        "bo",
+        "bo",
+    ),
+    (
+        "da",
+        "da",
+    ),
+    (
+        "de",
+        "de",
+    ),
+    ("de_DE", "de_DE"),
+    (
+        "el",
+        "el",
+    ),
+    ("es_419", "es_419"),
+    ("es_ES", "es_ES"),
+    (
+        "fa",
+        "fa",
+    ),
+    (
+        "fr",
+        "fr",
+    ),
+    ("fr_CA", "fr_CA"),
+    (
+        "he",
+        "he",
+    ),
+    (
+        "hi",
+        "hi",
+    ),
+    (
+        "hu",
+        "hu",
+    ),
+    (
+        "id",
+        "id",
+    ),
+    ("it_IT", "it_IT"),
+    (
+        "ja",
+        "ja",
+    ),
+    (
+        "ka",
+        "ka",
+    ),
+    (
+        "kk",
+        "kk",
+    ),
+    (
+        "ko",
+        "ko",
+    ),
+    (
+        "lv",
+        "lv",
+    ),
+    (
+        "nl",
+        "nl",
+    ),
+    (
+        "pl",
+        "pl",
+    ),
+    ("pt_BR", "pt_BR"),
+    ("pt_PT", "pt_PT"),
+    (
+        "ro",
+        "ro",
+    ),
+    (
+        "ru",
+        "ru",
+    ),
+    (
+        "sq",
+        "sq",
+    ),
+    (
+        "sv",
+        "sv",
+    ),
+    (
+        "sw",
+        "sw",
+    ),
+    (
+        "te",
+        "te",
+    ),
+    (
+        "th",
+        "th",
+    ),
+    ("tr_TR", "tr_TR"),
+    (
+        "uk",
+        "uk",
+    ),
+    (
+        "uz",
+        "uz",
+    ),
+    (
+        "vi",
+        "vi",
+    ),
+    ("zh_CN", "zh_CN"),
+    ("zh_HK", "zh_HK"),
+]
+
+# Overrides for display names for the language codes.
+# If we have an override, we should display that, because the code isn't ISO 639-1
+# compliant. Otherwise, we should use the ISO 639-1 name.
+COURSE_VARIANT_LANGUAGE_OVERRIDE = {
+    "af_ZA": "Afrikaans - South Africa",
+    "de_DE": "Deutsch - Deutschland",
+    "es_419": "Spanisn - LATAM",
+    "es_ES": "Spanish - España",
+    "fr_CA": "French - Canada",
+    "it_IT": "Italian - Italy",
+    "pt_BR": "Portugese - Brazil",
+    "pt_PT": "Portugese - Portugal",
+    "tr_TR": "Turkish - Türkiye",
+    "zh_CN": "Chinese - China",
+    "zh_HK": "Chinese - Hong Kong",
+}
