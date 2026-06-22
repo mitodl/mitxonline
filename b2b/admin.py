272778e7b17f03c0e5a6d3011e07c9b7a0a05cb0
James Kachel <jkachel@mit.edu>
2026-06-22 08:23:49 -0500

    Update the discount redemption admin for assignment fields/status

--- b2b/admin.py
+++ b2b/admin.py
@@ -3,6 +3,7 @@
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from django.db.models import Count
+from django.utils.html import format_html
 
 from b2b.models import (
     ContractPage,
@@ -124,10 +125,19 @@ class ContractPageCourseRunInline(DisplayOnlyAdminMixin, admin.TabularInline):
 
 
 @admin.register(DiscountContractAttachmentRedemption)
-class DiscountContractAttachmentRedemptionAdmin(ReadOnlyModelAdmin):
+class DiscountContractAttachmentRedemptionAdmin(admin.ModelAdmin):
     """Admin for discount attachments."""
 
-    list_display = ["user", "contract", "discount", "created_on"]
+    list_display = [
+        "discount_code",
+        "status",
+        "user",
+        "assignee",
+        "contract",
+        "created_on",
+        "redeemed_on",
+        "last_reminder_sent_on",
+    ]
     list_filter = [
         (
             "user",
@@ -141,16 +151,66 @@ class DiscountContractAttachmentRedemptionAdmin(ReadOnlyModelAdmin):
             "user__user_organizations__organization",
             admin.RelatedOnlyFieldListFilter,
         ),
+        "assigned_email",
+        "assigned_name",
     ]
     date_hierarchy = "created_on"
-    fields = ["user", "contract", "discount", "created_on"]
-    readonly_fields = ["user", "contract", "discount", "created_on"]
+    fields = [
+        "user",
+        "assigned_name",
+        "assigned_email",
+        "assigned_by",
+        "contract",
+        "discount",
+        "created_on",
+        "redeemed_on",
+        "last_reminder_sent_on",
+    ]
+    readonly_fields = [
+        "user",
+        "contract",
+        "discount",
+        "assigned_by",
+        "created_on",
+        "redeemed_on",
+        "last_reminder_sent_on",
+    ]
     search_fields = [
         "user__email",
         "user__global_id",
         "contract__slug",
         "discount__discount_code",
+        "assigned_email",
+        "assigned_name",
     ]
+
+    @admin.display(description="Discount Code")
+    def discount_code(self, instance):
+        """Return the discount code"""
+
+        return instance.discount.discount_code
+
+    def status(self, instance):
+        """Return the current state of the instance."""
+
+        if not instance.user and not instance.assigned_email:
+            return "-"
+
+        states = []
+
+        if instance.user:
+            states.append("Redeemed")
+        if instance.assigned_email:
+            states.append("Assigned")
+
+        return format_html("<br />".join(states))
+
+    @admin.display(description="Assignee")
+    def assignee(self, instance):
+        """Return the assignee information, if available."""
+
+        namestr = f"<br />{instance.assigned_name}" if instance.assigned_name else ""
+        return format_html(f"{instance.assigned_email}{namestr}")
 
 
 @admin.register(ContractPage)
