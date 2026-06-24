896434f0dcb18c5b3efde28f9fdd6725a4676d57
dansubak <dansubak@mit.edu>
2026-06-24 10:32:41 -0400

    Remove max_page_size

--- b2b/views/v0/manager.py
+++ b2b/views/v0/manager.py
@@ -52,7 +52,6 @@ log = logging.getLogger(__name__)
 class ManagerContractOrgPagination(PageNumberPagination):
     page_size = 25
     page_size_query_param = "page_size"
-    max_page_size = 100
 
 
 @dataclass
