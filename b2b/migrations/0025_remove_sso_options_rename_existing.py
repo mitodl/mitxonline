2701f77e719e983555e37b439f1567ce885640c3
James Kachel <jkachel@mit.edu>
2026-06-30 09:34:46 -0500

    Fix filter for membership type change - "non-sso" vs. "nonsso"

--- b2b/migrations/0025_remove_sso_options_rename_existing.py
+++ b2b/migrations/0025_remove_sso_options_rename_existing.py
@@ -12,7 +12,7 @@ def forwards_func(apps, schema_editor):
         models.Q(integration_type="sso") | models.Q(membership_type="sso")
     ).update(membership_type="managed")
     ContractPage.objects.filter(
-        models.Q(integration_type="nonsso") | models.Q(membership_type="nonsso")
+        models.Q(integration_type="non-sso") | models.Q(membership_type="non-sso")
     ).update(membership_type="code")
 
 
