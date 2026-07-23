9e333c175e700910918613b9d17375ccc94731d5
James Kachel <jkachel@mit.edu>
2026-07-23 12:01:02 -0500

    fix seer issues

--- ecommerce/views/webhooks/__init__.py
+++ ecommerce/views/webhooks/__init__.py
@@ -13,11 +13,6 @@ from django.conf import settings
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from mitol.payment_gateway import api, constants
-from mitol.payment_gateway.exceptions import (
-    BadStripeWebhookSecretError,
-    ImproperStripeWebhookRequestError,
-    NoStripeWebhookSecretError,
-)
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -47,20 +42,10 @@ class StripeWebhookView(APIView):
         have to do it again later.
         """
 
-        try:
-            stripe_event = api.PaymentGateway.validate_processor_response(
-                constants.MITOL_PAYMENT_GATEWAY_STRIPE,
-                request,
-            )
-        except ImproperStripeWebhookRequestError:
-            msg = "Received a response from Stripe that was unusable."
-            log.exception(msg)
-            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        except (NoStripeWebhookSecretError, BadStripeWebhookSecretError):
-            msg = "Could not validate the signature for the request."
-            log.exception(msg)
-            return Response(status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
+        stripe_event = api.PaymentGateway.validate_processor_response(
+            constants.MITOL_PAYMENT_GATEWAY_STRIPE,
+            request,
+        )
         self.event = stripe_event
         return True
 
