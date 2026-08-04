7f97e5b7a57713ca7f42805b500006fd993f404d
Anna <annagav@mit.edu>
2026-08-04 08:40:13 -0400

    name fields not required on serializer

--- users/serializers.py
+++ users/serializers.py
@@ -99,8 +99,8 @@ class LegalAddressSerializer(serializers.ModelSerializer):
     # NOTE: the model defines these as allowing empty values for backwards compatibility
     #       so we override them here to require them for new writes
     country = serializers.CharField(max_length=2)
-    first_name = serializers.CharField(max_length=60)
-    last_name = serializers.CharField(max_length=60)
+    first_name = serializers.CharField(max_length=60, required=False, allow_blank=True)
+    last_name = serializers.CharField(max_length=60, required=False, allow_blank=True)
     street_address_1 = serializers.CharField(
         max_length=255, required=False, allow_blank=True
     )
