d9652dfe2bbeb1c4cdd14ad17699a534d38764a7
CP <collinp@mit.edu>
2026-08-24 15:18:35 -0400

    Sync HubSpot first and last names
    Include separate `firstname` and `lastname` fields when building HubSpot contact sync messages, and update the API test to cover the new mapped properties.

--- hubspot_sync/api.py
+++ hubspot_sync/api.py
@@ -1185,6 +1185,8 @@ def make_contact_sync_message_from_user(
     contact_properties_map = {
         "email": "email",
         "name": "name",
+        "first_name": "firstname",
+        "last_name": "lastname",
         "country": "country",
         "state": "state",
         "year_of_birth": "yearofbirth",
