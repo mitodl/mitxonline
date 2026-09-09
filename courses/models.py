55c210777a9af219a59e3bc1fccbe0c09416aed2
James Kachel <jkachel@mit.edu>
2026-09-09 16:04:06 -0500

    Add a FK to ContractPage for ProgramEnrollment and CourseRunEnrollment, and a backfill migration
    It will become harder to figure out whether or not a particular enrollment is for a B2B run or not, once we allow course runs to be in multiple contracts. So, adding a field to track that specifically (to be followed by API changes to ensure that the field is getting set properly).

--- courses/models.py
+++ courses/models.py
@@ -2258,6 +2258,13 @@ class CourseRunEnrollment(EnrollmentModel):
             "longer retried automatically."
         ),
     )
+    b2b_contract = models.ForeignKey(
+        "b2b.ContractPage",
+        on_delete=models.DO_NOTHING,
+        related_name="course_run_enrollments",
+        null=True,
+        blank=True,
+    )
 
     objects = ActiveCourseRunEnrollmentManager()
     all_objects = CourseRunEnrollmentManager()
@@ -2395,6 +2402,13 @@ class ProgramEnrollment(EnrollmentModel):
 
     program = models.ForeignKey(
         "courses.Program", on_delete=models.CASCADE, related_name="enrollments"
+    )
+    b2b_contract = models.ForeignKey(
+        "b2b.ContractPage",
+        on_delete=models.DO_NOTHING,
+        related_name="program_enrollments",
+        null=True,
+        blank=True,
     )
 
     objects = ActiveProgramEnrollmentManager()
