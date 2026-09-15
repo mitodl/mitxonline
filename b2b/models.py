5a0ac54786e721c70e2a343b600215b641c9d082
dansubak <dansubak@mit.edu>
2026-09-15 15:02:58 -0400

    Add migration to add data consent boolean, timestamps

--- b2b/models.py
+++ b2b/models.py
@@ -995,7 +995,7 @@ def is_organization_manager(user, org_id):
     ).exists()
 
 
-class UserB2BContract(models.Model):
+class UserB2BContract(TimestampedModel):
     user = models.ForeignKey(
         settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
@@ -1006,6 +1006,8 @@ class UserB2BContract(models.Model):
         on_delete=models.CASCADE,
         related_name="b2b_contract_users",
     )
+    consented_to_data_sharing = models.BooleanField(null=True)
+    consent_modified_at = models.DateTimeField(null=True, blank=True)
 
     class Meta:
         unique_together = ("user", "contract_page")
